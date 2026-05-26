20d9038a8741f039415d9ee55a43a6c8ffcb33a5
eunnn <ehddms0517@naver.com>
2026-05-26 20:41:42 +0900

    test: 상품 가격 테스트 코드 추가
    Co-authored-by: JuHyeong424 <JuHyeong424@users.noreply.github.com>

--- server/__tests__/domain/products.test.ts
+++ server/__tests__/domain/products.test.ts
@@ -2,18 +2,23 @@ import { describe, expect, test } from '@jest/globals';
 import Product from '../../src/domain/Product';
 
 describe('상품 도메인 테스트', () => {
+  //given
+  const data = {
+    name: '나이키',
+    price: 1000,
+    image: 'img/test',
+  };
+
+  //when
+  const product = new Product(data);
+
   test('상품명은 1자 이상 100자 이하이다.', () => {
-    //given
-    const data = {
-      name: '나이키',
-      price: 1000,
-      image: 'img/test',
-    };
-
-    //when
-    const product = new Product(data);
-
     //then
     expect(product.getProduct().name).toBe('나이키');
+  });
+
+  test('가격은 0보다 큰 숫자이다.', () => {
+    //then
+    expect(product.getProduct().price).toBe(1000);
   });
 });
