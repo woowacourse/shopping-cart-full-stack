b5d3c0166151bb6717f95af573df8ed8654c4441
eunnn <ehddms0517@naver.com>
2026-05-26 20:36:28 +0900

    feat: Product 객체 생성
    Co-authored-by: JuHyeong424 <JuHyeong424@users.noreply.github.com>

--- server/__tests__/domain/products.test.ts
+++ server/__tests__/domain/products.test.ts
@@ -1,4 +1,5 @@
 import { describe, expect, test } from '@jest/globals';
+import Product from '../../src/domain/Product';
 
 describe('상품 도메인 테스트', () => {
   test('상품명은 1자 이상 100자 이하이다.', () => {
---
+++ server/src/domain/Product.ts
@@ -0,0 +1,30 @@
+export default class Product {
+  private name: string;
+  private price: number;
+  private image?: string;
+  private id: string;
+
+  constructor({
+    name,
+    price,
+    image,
+  }: {
+    name: string;
+    price: number;
+    image: string;
+  }) {
+    this.name = name;
+    this.price = price;
+    this.image = image;
+    this.id = crypto.randomUUID();
+  }
+
+  getProduct() {
+    return {
+      name: this.name,
+      price: this.price,
+      image: this.image,
+      id: this.id,
+    };
+  }
+}
