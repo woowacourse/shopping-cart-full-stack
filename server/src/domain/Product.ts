0ff314df79e1441e510c0cc7f3ca4c94df492750
eunnn <ehddms0517@naver.com>
2026-05-26 21:12:35 +0900

    feat: 가격 0원 이하 예외 처리
    Co-authored-by: JuHyeong424 <JuHyeong424@users.noreply.github.com>

--- server/src/domain/Product.ts
+++ server/src/domain/Product.ts
@@ -14,6 +14,7 @@ export default class Product {
     image?: string;
   }) {
     this.#validatorName(name);
+    this.#validatorPrice(price);
     this.name = name;
     this.price = price;
     this.image = image;
@@ -24,6 +25,10 @@ export default class Product {
     if (name.length === 0) throw new Error('상품명이 공백입니다.');
 
     if (name.length > 100) throw new Error('상품명이 100자를 초과합니다.');
+  }
+
+  #validatorPrice(price: number) {
+    if (price <= 0) throw new Error('가격은 1원 이상입니다.');
   }
 
   getProduct() {
