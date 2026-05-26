d7cd0d9b1bd3b75b7d42c787c664ca1a38c63d13
binggwa <qudrhks317@gmail.com>
2026-05-26 14:08:31 +0900

    chore: 환경설정

--- server/src/app.ts
+++ server/src/app.ts
@@ -7,4 +7,23 @@ app.get("/health", (_req, res) => {
   res.json({ status: "ok" });
 });
 
+app.get("/", (req, res) => {
+  res.send("Hello World");
+});
+
+app.get("/html-test", (req, res) => {
+    res.send(`
+        <html>
+        <head>
+            <script>
+                console.log("Hello World");
+            </script>
+        </head>
+        <body>
+            <h1>Hello World</h1>
+        </body>
+        </html>
+    `);
+});
+
 export default app;
