48a6b75341659c6412149a6ab47cff431514d257
eunnn <ehddms0517@naver.com>
2026-05-26 21:17:59 +0900

    test: 가격이 숫자가 아닌 경우 테스트 코드 추가
    Co-authored-by: JuHyeong424 <JuHyeong424@users.noreply.github.com>

--- server/__tests__/domain/products.test.ts
+++ server/__tests__/domain/products.test.ts
@@ -53,4 +53,14 @@ describe('상품 도메인 예외 테스트', () => {
 
     expect(() => new Product(data)).toThrow('가격은 1원 이상입니다.');
   });
+
+  test('가격이 숫자가 아니면 예외 처리한다.', () => {
+    const data = {
+      name: '나이키',
+      price: 'abc' as unknown as number,
+      image: 'img/test',
+    };
+
+    expect(() => new Product(data)).toThrow('가격은 숫자입니다.');
+  });
 });
