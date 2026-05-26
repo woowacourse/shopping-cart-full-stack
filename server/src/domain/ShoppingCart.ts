9259863a47a1e5e48bb659241db412100c507a63
eunnn <ehddms0517@naver.com>
2026-05-26 21:32:58 +0900

    feat: 상품 수량 0 이하인 경우 예외 처리
    Co-authored-by: JuHyeong424 <JuHyeong424@users.noreply.github.com>

--- server/src/domain/ShoppingCart.ts
+++ server/src/domain/ShoppingCart.ts
@@ -3,8 +3,13 @@ export default class ShoppingCart {
   private quantity: number;
 
   constructor({ id, quantity }: { id: string; quantity: number }) {
+    this.#validateQuantity(quantity);
     this.id = id;
     this.quantity = quantity;
+  }
+
+  #validateQuantity(quantity: number) {
+    if (quantity < 1) throw new Error('상품 수량이 1 이상이어야 합니다.');
   }
 
   getShoppingCart() {
