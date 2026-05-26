06130b0a4a564955fb15d7169eaf62cfb6346b65
binggwa <qudrhks317@gmail.com>
2026-05-26 20:05:52 +0900

    feat: Cart와 Product 타입 인터페이스 정의
    Co-authored-by: yuncic <ksy151020@gmail.com>

--- server/src/repositories/CartRepository.ts
+++ server/src/repositories/CartRepository.ts
@@ -1,59 +1,10 @@
-import { StoredCartItem } from "./CartItem";
-export default class CartRepository {
-  #cart: Map<number, StoredCartItem>;
-  #nextId: number;
-
-  constructor() {
-    this.#cart = new Map();
-    this.#nextId = 1;
-  }
-
-  findById(cartItemId: number): StoredCartItem | null {
-    return this.#cart.get(cartItemId) ?? null;
-  }
-
-  getCartProducts(): StoredCartItem[] {
-    return [...this.#cart.values()];
-  }
-
-  addProductToCart(productId: number, quantity: number): StoredCartItem {
-    const cartItem = {
-      cartItemId: this.#nextId,
-      quantity,
-      productId,
+export interface Cart {
+    cartItemId: number;
+    quantity: number;
+    data: {
+        productId: number;
+        name: string;
+        price: number;
+        thumbnailUrl: string;
     };
-    this.#cart.set(this.#nextId, cartItem);
-    this.#nextId++;
-    return cartItem;
-  }
-
-  changeQuantity(
-    cartItemId: number,
-    newQuantity: number,
-  ): StoredCartItem | null {
-    const cartItem = this.#cart.get(cartItemId);
-    if (!cartItem) return null;
-
-    const newItem = { ...cartItem, quantity: newQuantity };
-    this.#cart.set(cartItemId, newItem);
-
-    return newItem;
-  }
-
-  deleteByCartId(cartItemId: number): void {
-    this.#cart.delete(cartItemId);
-  }
-
-  deleteByProductId(productId: number): void {
-    [...this.#cart.entries()]
-      .filter(([_, StoredCartItem]) => StoredCartItem.productId === productId)
-      .forEach(([cartItemId]) => this.#cart.delete(cartItemId));
-  }
-
-  clear(): void {
-    this.#cart.clear();
-    this.#nextId = 1;
-  }
 }
-
-export const cartRepository = new CartRepository();
