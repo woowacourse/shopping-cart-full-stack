18466672d741372ee865fbde89583ec57cf53a84
binggwa <qudrhks317@gmail.com>
2026-05-27 19:00:07 +0900

    feat: cartRouter, productRouter에 들어갈 컨트롤러 메서드 수정
    Co-authored-by: yuncic <ksy151020@gmail.com>

--- server/src/routes/ProductRouter.ts
+++ server/src/routes/ProductRouter.ts
@@ -1,14 +1,15 @@
 import { Router } from "express";
+import productController from "../controller/ProductController.ts"
 
 const productRouter = Router();
 
 // GET /products
-productRouter.get("/", productController.getProducts());
+productRouter.get("/", productController.getProducts);
 
 // POST /products
-productRouter.post("/", productController.postProducts());
+productRouter.post("/", productController.postProducts);
 
 // DELETE /products/:productId
-productRouter.delete("/:productId", productController.deleteProducts());
+productRouter.delete("/:productId", productController.deleteProducts);
 
 export default productRouter;
