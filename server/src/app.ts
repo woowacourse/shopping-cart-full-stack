314eda43853a549258f43145fe020783ff4899c2
eunnn <ehddms0517@naver.com>
2026-05-27 18:50:17 +0900

    feat: 상품 추가 기능 구현
    Co-authored-by: JuHyeong424 <JuHyeong424@users.noreply.github.com>

--- server/src/app.ts
+++ server/src/app.ts
@@ -1,12 +1,17 @@
 import express from 'express';
 
-import { getAllProducts } from './service/productService.ts';
+import { getAllProducts, createProduct } from './service/productService.ts';
 
 const app = express();
 app.use(express.json());
 
 app.get('/products', (_req, res) => {
   res.status(200).send(getAllProducts());
+});
+
+app.post('/products', (req, res) => {
+  const request = req.body;
+  res.status(201).send(createProduct(request));
 });
 
 export default app;
