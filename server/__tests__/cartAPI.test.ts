a58b4b369408e52a8ba027bd0afffa0a75e0ac3a
hjkim0905 <eax9952@gmail.com>
2026-05-27 18:19:55 +0900

    test(API): Cart Post API 테스트 구현
    Co-authored-by: lee-eojin <lejin0906@gmail.com>

--- server/__tests__/cartAPI.test.ts
+++ server/__tests__/cartAPI.test.ts
@@ -36,3 +36,31 @@ describe('GET /cart', function () {
     expect(response.body).toEqual({ errorMessage: '서버에 일시적인 오류가 발생했습니다.' });
   });
 });
+
+app.post('/cart/:id', function (req: Request, res: Response) {
+  const requestId = Number(req.params.id);
+
+  if (!TestDB.Cart) {
+    return res.status(500).json({ errorMessage: '서버에 일시적인 오류가 발생했습니다.' });
+  }
+  const pickedProduct = TestDB.Products!.filter((product) => product.id === requestId)[0];
+  TestDB.Cart.push(pickedProduct);
+  res.status(201).json({ message: '상품이 장바구니에 추가되었습니다.' });
+});
+
+describe('POST /cart/:id', function () {
+  it('새로운 상품이 등록되고, 응답 코드는 201 Created 이다.', async function () {
+    const response = await request(app).post('/cart/1').set('Accept', 'application/json');
+
+    expect(response.status).toBe(201);
+  });
+
+  it('CART 테이블에 존재하지 않으면 500 에러', async function () {
+    TestDB.Cart = undefined;
+
+    const response = await request(app).post('/cart/1').set('Accept', 'application/json');
+
+    expect(response.status).toBe(500);
+    expect(response.body).toEqual({ errorMessage: '서버에 일시적인 오류가 발생했습니다.' });
+  });
+});
