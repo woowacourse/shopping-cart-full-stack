e4f26ce4962d3fb152a65e3b167ebd2181f3ae38
eunnn <ehddms0517@naver.com>
2026-05-27 18:50:03 +0900

    test: 상품 추가 테스트 코드 추가
    Co-authored-by: JuHyeong424 <JuHyeong424@users.noreply.github.com>

--- server/__tests__/api/app.test.ts
+++ server/__tests__/api/app.test.ts
@@ -2,21 +2,34 @@ import request from 'supertest';
 import app from '../../src/app';
 import { describe, expect, test } from '@jest/globals';
 
-import {
-  createProduct,
-  getAllProducts,
-} from '../../src/service/productService';
-
-const data = {
-  name: 'test',
-  price: 1000,
-  image: 'example/com',
-};
-
-createProduct(data);
+import { getAllProducts } from '../../src/service/productService';
 
 describe('상품 GET API 테스트', () => {
+  test('클라이언트가 POST 요청 시 상품을 등록한다.', async () => {
+    const data = {
+      name: 'test',
+      price: 1000,
+      image: 'example/com',
+    };
+
+    const response = await request(app).post('/products').send(data);
+
+    expect(response.status).toBe(201);
+    expect(response.body).toEqual([
+      {
+        id: getAllProducts()[0].getProduct().id,
+        ...data,
+      },
+    ]);
+  });
+
   test('클라이언트가 GET 요청 시 상품 목록을 반환한다.', async () => {
+    const data = {
+      name: 'test',
+      price: 1000,
+      image: 'example/com',
+    };
+
     const response = await request(app).get('/products');
 
     expect(response.status).toBe(200);
