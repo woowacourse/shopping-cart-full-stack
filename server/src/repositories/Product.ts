f8a21cc051d4cd28caf26e1798738d9c02a8c71f
binggwa <qudrhks317@gmail.com>
2026-05-27 15:10:16 +0900

    feat: ProductData와 검증함수를 담는 Product 추가
    Co-authored-by: yuncic <ksy151020@gmail.com>

---
+++ server/src/repositories/Product.ts
@@ -0,0 +1,21 @@
+export interface ProductData {
+  productId: number;
+  name: string;
+  price: number;
+  thumbnailUrl: string;
+  totalQuantity: number;
+}
+
+export type ProductInput = Omit<ProductData, 'productId'>;
+
+export const validateProductData = (data: ProductInput): void => {
+  if (data.totalQuantity < 1 || data.totalQuantity > 99) {
+    throw new Error("quantity는 1~99 사이어야합니다.");
+  }
+  if (data.name.length > 100) {
+    throw new Error("name은 100자 이내여야합니다.");
+  }
+  if (data.price <= 0) {
+    throw new Error("price는 0보다 큰 숫자이어야합니다.");
+  }
+};
