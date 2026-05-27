d3b23d2a28f7e13fcbcbbed3b81badf277752296
박건규 <geongyu09@gmail.com>
2026-05-27 21:14:44 +0900

    feat: 상품 저장 repository 함수 구현
    Co-authored-by: Hyewon Jang <janghw232625@gmail.com>

--- server/src/modules/products/products.repository.ts
+++ server/src/modules/products/products.repository.ts
@@ -1,8 +1,31 @@
+import type { Product } from "@/type";
 import { ProductDB } from "@db/inMemoryDB";
+
+const makeCrateId = () => {
+  const lastCount = { count: 0 };
+
+  return function () {
+    return lastCount.count++;
+  };
+};
+
+const createId = makeCrateId();
 
 export const getAllProductsQuery = () => {
   // 상품 목록 조회
   const products = [...ProductDB.values()];
 
   return products;
+};
+
+export const addProductQuery = (product: Omit<Product, "id">) => {
+  const id = createId();
+  const newProducts: Product = {
+    id,
+    ...product,
+  };
+
+  ProductDB.set(id, newProducts);
+
+  return newProducts;
 };
