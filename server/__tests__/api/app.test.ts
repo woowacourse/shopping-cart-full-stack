40079ffb5abe796e7b956c636324aca0f005c0ad
eunnn <ehddms0517@naver.com>
2026-05-27 19:53:16 +0900

    test: 장바구니 상품 삭제 테스트 코드
    Co-authored-by: JuHyeong424 <JuHyeong424@users.noreply.github.com>

--- server/__tests__/api/app.test.ts
+++ server/__tests__/api/app.test.ts
@@ -10,6 +10,7 @@ import {
   createShoppingCart,
   patchShoppingCart,
   getShoppingCart,
+  deleteShoppingCart,
 } from '../../src/service/shoppingCartService';
 
 describe('상품 API 테스트', () => {
@@ -81,7 +82,7 @@ describe('장바구니 상품 API 테스트', () => {
     ]);
   });
 
-  test('클라이언트가 patch 요청 시 상품 수량을 변경한다', async () => {
+  test('클라이언트가 PATCH 요청 시 상품 수량을 변경한다', async () => {
     const id = getAllProducts()[0].getProduct().id;
 
     patchShoppingCart(id, 4);
@@ -92,5 +93,16 @@ describe('장바구니 상품 API 테스트', () => {
 
     expect(response.status).toBe(204);
     expect(getShoppingCart()[0].quantity).toBe(4);
+  });
+
+  test('클라이언트가 DELETE 요청 시 해당 상품을 삭제한다.', async () => {
+    const id = getAllProducts()[0].getProduct().id;
+
+    deleteShoppingCart(id);
+
+    const response = await request(app).delete(`/carts/${id}`);
+
+    expect(response.status).toBe(204);
+    expect(getShoppingCart()).toEqual([]);
   });
 });
