863e64be9401440a14fb87c826167b8bdff76fb2
hjkim0905 <eax9952@gmail.com>
2026-05-27 21:56:46 +0900

    feat: express CORS 설정 구현
    Co-authored-by: lee-eojin <lejin0906@gmail.com>

--- server/src/app.ts
+++ server/src/app.ts
@@ -1,10 +1,14 @@
-import express from 'express';
+import express, { Request, Response } from 'express';
 
 const app = express();
-app.use(express.json());
 
-app.get('/health', (_req, res) => {
-  res.json({ status: 'ok' });
+app.use((req: Request, res: Response, next) => {
+  res.header('Access-Control-Allow-Origin', 'http://lcoalhost:3000');
+  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+  next();
 });
+
+app.listen(3000);
 
 export default app;
