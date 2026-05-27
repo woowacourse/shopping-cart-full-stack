2c1c221ecf157effcb30da9a179318c1a7f45be4
GGGGGGGGGGGGGGG_J <115770858+NaturalSoda4552@users.noreply.github.com>
2026-05-27 19:19:15 +0900

    feat: userDB에 cartItemsDB 추가
    Co-authored-by: TH_97 <TH-97@users.noreply.github.com>

--- server/src/db.ts
+++ server/src/db.ts
@@ -1,3 +1,11 @@
+import { CartItem } from './modules/cart/cartItem.model.js';
 import { Product } from './modules/products/product.model.js';
 
 export const productsDB = new Map<string, Product>();
+
+export const userDB = [
+  {
+    userId: '1',
+    cartItemsDB: new Map<string, CartItem>(),
+  },
+];
