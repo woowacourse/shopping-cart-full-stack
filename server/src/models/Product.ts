93b26164726182f97d8160f4776e0c3f293a8939
Antoliny0919 <antoliny0919@gmail.com>
2026-05-27 15:12:33 +0900

    fix: Product 모델에 id 필드 추가
    Co-authored-by: boboyoii <boyeon6331@gmail.com>

--- server/src/models/Product.ts
+++ server/src/models/Product.ts
@@ -1,9 +1,11 @@
 class Product {
+  #id: string;
   #name: string;
   #price: number;
   #thumbnail: string;
 
   constructor(name: string, price: number, thumbnail: string) {
+    this.#id = crypto.randomUUID();
     this.#name = name;
     this.#price = price;
     this.#thumbnail = thumbnail;
