a18e8ec22b54fb833937875768da930cd3b8c624
eunnn <ehddms0517@naver.com>
2026-05-27 15:23:09 +0900

    test: 상품 삭제 테스트 코드 추가
    Co-authored-by: JuHyeong424 <JuHyeong424@users.noreply.github.com>

--- server/__tests__/domain/shoppingCart.test.ts
+++ server/__tests__/domain/shoppingCart.test.ts
@@ -4,7 +4,7 @@ import ShoppingCart from '../../src/domain/ShoppingCart';
 describe('쇼핑 장바구니 도메인 테스트', () => {
   test('쇼핑 장바구니의 수량은 1 이상 99 이하의 정수이다.', () => {
     const data = {
-      id: 'testId',
+      productId: 'testId',
       quantity: 3,
     };
 
@@ -16,47 +16,63 @@ describe('쇼핑 장바구니 도메인 테스트', () => {
 
   test('해당 장바구니 상품의 수량을 증가시킨다.', () => {
     const data1 = {
-      id: 'testId1',
+      productId: 'testId1',
       quantity: 3,
     };
 
     const data2 = {
-      id: 'testId1',
+      productId: 'testId1',
       quantity: 5,
     };
 
     const shoppingCart = new ShoppingCart();
     shoppingCart.add(data1);
-    shoppingCart.setQuantity(data2.id, data2.quantity);
+    shoppingCart.setQuantity(data2.productId, data2.quantity);
 
     expect(shoppingCart.getQuantity('testId1')).toBe(5);
   });
 
   test('해당 장바구니 상품의 수량을 감소시킨다.', () => {
     const data1 = {
-      id: 'testId1',
+      productId: 'testId1',
       quantity: 3,
     };
 
     const data2 = {
-      id: 'testId1',
+      productId: 'testId1',
       quantity: 1,
     };
 
     const shoppingCart = new ShoppingCart();
     shoppingCart.add(data1);
-    shoppingCart.setQuantity(data2.id, data2.quantity);
+    shoppingCart.setQuantity(data2.productId, data2.quantity);
 
     expect(shoppingCart.getQuantity('testId1')).toBe(1);
   });
 
-  // 3. 상품 삭제할 때
+  test('해당 장바구니 상품을 삭제한다.', () => {
+    const data1 = {
+      productId: 'testId1',
+      quantity: 3,
+    };
+    const data2 = {
+      productId: 'testId2',
+      quantity: 4,
+    };
+
+    const shoppingCart = new ShoppingCart();
+    shoppingCart.add(data1);
+    shoppingCart.add(data2);
+    shoppingCart.deleteProduct('testId1');
+
+    expect(shoppingCart.getShoppingCart()).toEqual([data2]);
+  });
 });
 
 describe('쇼핑 장바구니 도메인 예외 테스트', () => {
   test('장바구니 상품 수량이 0 이하이면 예외 처리한다.', () => {
     const data = {
-      id: 'testId',
+      productId: 'testId',
       quantity: 0,
     };
 
@@ -69,7 +85,7 @@ describe('쇼핑 장바구니 도메인 예외 테스트', () => {
 
   test('장바구니 상품 수량이 100 이상이면 예외 처리한다.', () => {
     const data = {
-      id: 'testId',
+      productId: 'testId',
       quantity: 100,
     };
 
