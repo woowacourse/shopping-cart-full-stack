bfc7a61ebb2a48c8b65d6c7b3f55057f2be954fb
박건규 <geongyu09@gmail.com>
2026-05-27 21:16:19 +0900

    feat: 제품 추가 서비스 로직 작성
    Co-authored-by: Hyewon Jang <janghw232625@gmail.com>

--- server/src/modules/products/products.service.ts
+++ server/src/modules/products/products.service.ts
@@ -1,5 +1,11 @@
-import { getAllProductsQuery } from "./products.repository";
+import { validateProductRules } from "./products.validator";
+import { addProductQuery, getAllProductsQuery } from "./products.repository";
 
 export const getAllProducts = () => {
   return getAllProductsQuery();
+};
+
+export const addProduct = (arg: Parameters<typeof addProductQuery>[0]) => {
+  validateProductRules(arg);
+  return addProductQuery(arg);
 };
