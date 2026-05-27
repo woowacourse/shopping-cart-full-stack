7cbd9f4c8eb49f18c7a787958de27b7602bf3fe8
binggwa <qudrhks317@gmail.com>
2026-05-27 15:10:41 +0900

    feat: ProductRepository를 통해 ProductData를 담는 상품 목록 추가
    Co-authored-by: yuncic <ksy151020@gmail.com>

--- server/src/repositories/ProductRepository.ts
+++ server/src/repositories/ProductRepository.ts
@@ -1,8 +1,42 @@
-export interface Product {
-  productId: number;
-  name: string;
-  price: number;
-  thumbnailUrl: string;
-  quantity: number;
-  totalQuantity: number;
+import type { ProductData, ProductInput } from "./Product.ts";
+import { validateProductData } from "./Product.ts";
+
+class ProductRepository {
+  // Product의 데이터를 담고 있는 private 변수
+  // Map 형태에 넣어주기 위한 index로 nextId
+  #products: Map<number, ProductData>;
+  #nextId: number;
+
+  // 생성자
+  // 생성자는  productId를 만들고, Product 데이터를 담은 Map을 생성한다.
+  constructor() {
+    this.#products = new Map();
+    this.#nextId = 1;
+  }
+
+  // `getProducts()` 메서드를 통해 전체 목록을 조회가능.
+  getProducts(): ProductData[] {
+    return [...this.#products.values()];
+  }
+
+  // `findById()` 메서드를 통해 상품 ID로 단건 조회가능.
+  findById(productId: number): ProductData | null {
+    return this.#products.get(productId) ?? null;
+  }
+
+  // `addProduct()` 메서드를 통해 상품 목록을 추가가능.
+  addProduct(data: ProductInput): ProductData {
+    validateProductData(data);
+    const product = { ...data, productId: this.#nextId };
+    this.#products.set(this.#nextId, product);
+    this.#nextId++;
+    return product;
+  }
+
+  // `deleteById()` 메서드를 통해 상품 목록에서 제거가능.
+  deleteById(productId: number): void {
+    this.#products.delete(productId);
+  }
 }
+
+export default ProductRepository;
