86235dfddd47950b86f6e27dfb682ad45e6c4329
binggwa <qudrhks317@gmail.com>
2026-05-27 20:40:44 +0900

    feat: cartController 기능 구현
    Co-authored-by: yuncic <ksy151020@gmail.com>

--- server/src/controller/CartController.ts
+++ server/src/controller/CartController.ts
@@ -1,52 +1,94 @@
 import { Request, Response } from "express";
-import {
-  getCartItemsService,
-  deleteCartItemService,
-  postCartItemService,
-  patchCartItemService,
-} from "../service/CartService";
-import { handleError } from "./ErrorHandler";
+import { cartRepository } from "../repositories/CartRepository";
+import { productRepository } from "../repositories/ProductRepository";
 
-const getCartItems = (_request: Request, response: Response): void => {
-  try {
-    const cartItems = getCartItemsService();
-    response.status(200).json(cartItems);
-  } catch (error) {
-    handleError(response, error);
-  }
+const getCartItems = (_request: Request, response: Response): Response => {
+  const cartItems = cartRepository.getCartProducts();
+  return response.status(200).json(cartItems);
 };
 
-const postCartItem = (request: Request, response: Response): void => {
+const postCartItem = (request: Request, response: Response): Response => {
   try {
     const { productId, quantity } = request.body;
-    const addedCartItem = postCartItemService(
-      Number(productId),
-      Number(quantity),
-    );
-    response.status(201).json(addedCartItem);
+    const product = productRepository.findById(Number(productId));
+
+    if (!product) {
+      return response
+        .status(404)
+        .json({ message: "해당 상품이 존재하지 않습니다." });
+    }
+    const addedCartItem = cartRepository.addProductToCart(Number(productId), Number(quantity));
+    return response.status(201).json(addedCartItem);
   } catch (error) {
-    handleError(response, error);
+    if (error instanceof Error) {
+      return response.status(400).json({ message: "잘못된 요청 형식입니다." });
+    }
+    return response
+      .status(500)
+      .json({ message: "네트워크 에러가 발생했습니다!" });
   }
 };
 
-const deleteCartItem = (request: Request, response: Response): void => {
+const deleteCartItem = (request: Request, response: Response): Response => {
   try {
     const cartItemId = Number(request.params.cartItemId);
-    deleteCartItemService(cartItemId);
-    response.status(204).send();
+    if (!cartItemId) {
+      return response
+        .status(400)
+        .json({ message: "유효하지 않은 장바구니 ID입니다." });
+    }
+
+    const cartItem = cartRepository.findById(cartItemId);
+    if (!cartItem) {
+      return response
+        .status(404)
+        .json({ message: "해당 장바구니 상품이 존재하지 않습니다." });
+    }
+
+    cartRepository.deleteByCartId(cartItemId);
+
+    return response.status(204).send();
   } catch (error) {
-    handleError(response, error);
+    return response
+      .status(500)
+      .json({ message: "네트워크 에러가 발생했습니다!" });
   }
 };
 
-const patchCartItem = (request: Request, response: Response): void => {
+const patchCartItem = (request: Request, response: Response): Response => {
   try {
     const cartItemId = Number(request.params.cartItemId);
+    if (!cartItemId) {
+      return response
+        .status(400)
+        .json({ message: "유효하지 않은 장바구니 ID입니다." });
+    }
+
     const newQuantity = Number(request.body.quantity);
-    const patchedCartItem = patchCartItemService(cartItemId, newQuantity);
-    response.status(200).json(patchedCartItem);
+    if (!newQuantity) {
+      return response
+        .status(400)
+        .json({ message: "유효하지 않은 수량입니다." });
+    }
+
+    const patchedCartItem = cartRepository.changeQuantity(
+      cartItemId,
+      newQuantity,
+    );
+    if (!patchedCartItem) {
+      return response
+        .status(404)
+        .json({ message: "해당 장바구니 상품이 존재하지 않습니다." });
+    }
+
+    return response.status(200).json(patchedCartItem);
   } catch (error) {
-    handleError(response, error);
+    if (error instanceof Error) {
+      return response.status(400).json({ message: "잘못된 요청 형식입니다." });
+    }
+    return response
+      .status(500)
+      .json({ message: "네트워크 에러가 발생했습니다!" });
   }
 };
 
