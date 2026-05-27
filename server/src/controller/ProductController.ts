aa172a7fb2cb1b6db5d527493e2fa4cc0694a5ad
binggwa <qudrhks317@gmail.com>
2026-05-27 19:03:13 +0900

    feat: productController 기능 구현
    Co-authored-by: yuncic <ksy151020@gmail.com>

--- server/src/controller/ProductController.ts
+++ server/src/controller/ProductController.ts
@@ -1,37 +1,51 @@
 import { Request, Response } from "express";
-import { handleError } from "./ErrorHandler";
-import {
-  deleteProductsService,
-  getProductsService,
-  postProductsService,
-} from "../service/ProductService";
+import { productRepository } from "../repositories/ProductRepository";
+import { cartRepository } from "../repositories/CartRepository";
 
-const getProducts = (_request: Request, response: Response): void => {
+const getProducts = (_request: Request, response: Response): Response => {
+  const products = productRepository.getProducts();
+  return response.status(200).json(products);
+};
+
+const postProducts = (request: Request, response: Response): Response => {
   try {
-    const products = getProductsService();
-    response.status(200).json(products);
+    const addedProduct = productRepository.addProduct(request.body);
+
+    return response.status(201).json(addedProduct);
   } catch (error) {
-    handleError(response, error);
+    if (error instanceof Error) {
+      return response.status(400).json({ message: "잘못된 요청 형식입니다." });
+    }
+    return response
+      .status(500)
+      .json({ message: "네트워크 에러가 발생했습니다!" });
   }
 };
 
-const postProducts = (request: Request, response: Response): void => {
-  try {
-    const newProducts = request.body;
-    const addedProduct = postProductsService(newProducts);
-    response.status(201).json(addedProduct);
-  } catch (error) {
-    handleError(response, error);
-  }
-};
-
-const deleteProducts = (request: Request, response: Response): void => {
+const deleteProducts = (request: Request, response: Response): Response => {
   try {
     const productId = Number(request.params.productId);
-    deleteProductsService(productId);
-    response.status(204).send();
+    if (!productId) {
+      return response
+        .status(400)
+        .json({ message: "유효하지 않은 상품 ID입니다." });
+    }
+
+    const product = productRepository.findById(productId);
+    if (!product) {
+      return response
+        .status(404)
+        .json({ message: "해당 상품이 존재하지 않습니다." });
+    }
+
+    productRepository.deleteById(productId);
+    cartRepository.deleteByProductId(productId);
+    
+    return response.status(204).send();
   } catch (error) {
-    handleError(response, error);
+    return response
+      .status(500)
+      .json({ message: "네트워크 에러가 발생했습니다!" });
   }
 };
 
