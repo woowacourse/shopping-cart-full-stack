820ed82e580ae8df5792d532977a5d1a83fd7672
binggwa <qudrhks317@gmail.com>
2026-05-27 17:25:32 +0900

    chore: CORS 설정
    Co-authored-by: yuncic <ksy151020@gmail.com>

--- server/src/app.ts
+++ server/src/app.ts
@@ -1,7 +1,5 @@
 import express from "express";
 import cors from 'cors';
-import cartRouter from './routes/CartRouter';
-import productRouter from './routes/ProductRouter';
 
 const app = express();
 
