0b84dcb4451018546a9947d0ab5668a0cb094838
hjkim0905 <eax9952@gmail.com>
2026-05-27 19:50:32 +0900

    test(API): Cart PUT API 테스트 구현
    Co-authored-by: lee-eojin <lejin0906@gmail.com>

--- server/__tests__/cartAPI.test.ts
+++ server/__tests__/cartAPI.test.ts
@@ -1,6 +1,6 @@
 import express, { Request, Response } from 'express';
 import request from 'supertest';
-import { TestDB } from './testDB';
+import { BodyForTest, TestDB } from './testDB';
 
 const app = express();
 app.use(express.json());
@@ -37,6 +37,7 @@ describe('GET /cart', function () {
   });
 });
 
+// POST API
 app.post('/cart/:id', function (req: Request, res: Response) {
   const requestId = Number(req.params.id);
 
@@ -64,3 +65,74 @@ describe('POST /cart/:id', function () {
     expect(response.body).toEqual({ errorMessage: '서버에 일시적인 오류가 발생했습니다.' });
   });
 });
+
+// PUT API
+app.put('/cart', function (req: Request, res: Response) {
+  const { id } = req.body;
+
+  if (!TestDB.Cart) {
+    return res.status(500).json({ errorMessage: '서버에 일시적인 오류가 발생했습니다.' });
+  }
+
+  const toBeUpdatedIndex = TestDB.Cart.findIndex((product) => product.id === id);
+  if (toBeUpdatedIndex === -1) {
+    return res.status(404).json({ errorMessage: '상품을 찾을 수 없습니다.' });
+  }
+
+  TestDB.Cart[toBeUpdatedIndex] = req.body;
+  res.status(204).send();
+});
+
+const productInCart: BodyForTest = {
+  id: 1,
+  imageUrl: 'https://example.com/nike.jpg',
+  name: 'Nike Air Max',
+  price: 1200000,
+  quantity: 2,
+};
+
+const productToBeUpdated: BodyForTest = {
+  id: productInCart.id,
+  imageUrl: productInCart.imageUrl,
+  name: productInCart.name,
+  price: productInCart.price,
+  quantity: 4,
+};
+
+describe('PUT /cart', function () {
+  it('상품의 수량이 변경되고, 응답 코드는 204 OK 이다.', async function () {
+    const response = await request(app)
+      .put('/cart')
+      .send(productToBeUpdated)
+      .set('Accept', 'application/json');
+
+    expect(response.status).toBe(204);
+  });
+  it('CART 테이블에 존재하지 않으면 500 에러', async function () {
+    TestDB.Cart = undefined;
+
+    const response = await request(app)
+      .put('/cart')
+      .send(productToBeUpdated)
+      .set('Accept', 'application/json');
+
+    expect(response.status).toBe(500);
+    expect(response.body).toEqual({ errorMessage: '서버에 일시적인 오류가 발생했습니다.' });
+  });
+  it('해당 id가 DB에 존재하지 않는다면 404 에러', async function () {
+    const idChangedProduct = { ...productToBeUpdated };
+    idChangedProduct.id = 2;
+    const response = await request(app)
+      .put('/cart')
+      .send(idChangedProduct)
+      .set('Accept', 'application/json');
+
+    expect(response.status).toBe(404);
+    expect(response.body).toEqual({ errorMessage: '상품을 찾을 수 없습니다.' });
+  });
+});
+
+// const toBeUpdatedIndex = TestDB.Cart.findIndex(product => product.hasOwnProperty(productId));
+
+// id가 1인 인덱스가 2라고 가정
+// TestDB.Cart[toBeUpdatedIndex] = requestBody
