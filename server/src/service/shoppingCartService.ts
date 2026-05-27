164ce38110ef99d03ea4ed11a3786cb965b564cd
eunnn <ehddms0517@naver.com>
2026-05-27 16:56:48 +0900

    feat: 장바구니 목록 조회 시 상품을 반환
    Co-authored-by: JuHyeong424 <JuHyeong424@users.noreply.github.com>

--- server/src/service/shoppingCartService.ts
+++ server/src/service/shoppingCartService.ts
@@ -1,11 +1,15 @@
 import { shoppingCart } from '../database/inMemoryDatabase.ts';
+import { products } from '../database/inMemoryDatabase.ts';
 
 export function createShoppingCart(productId: string, quantity: number) {
   shoppingCart.add({ productId, quantity });
 }
 
 export function getShoppingCart() {
-  return shoppingCart.getShoppingCart();
+  const shoppingCartArray = shoppingCart.getShoppingCart();
+  return shoppingCartArray.map(({ productId, quantity }) => {
+    return { product: products.get(productId), quantity: quantity };
+  });
 }
 
 export function patchShoppingCart(productId: string, quantity: number) {
