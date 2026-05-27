e06e4198fed8c48fd2956e1ed37618e7a161e72e
박건규 <geongyu09@gmail.com>
2026-05-27 21:18:59 +0900

    feat: 제품 추가 컨트롤러 작성
    Co-authored-by: Hyewon Jang <janghw232625@gmail.com>

--- server/src/modules/products/products.controller.ts
+++ server/src/modules/products/products.controller.ts
@@ -1,14 +1,35 @@
 import { RequestHandler } from "express";
-import { getAllProducts } from "./products.service";
+import { validateProduct } from "./products.schema";
+import * as productsService from "./products.service";
 
 export const getProducts: RequestHandler = (_, res) => {
   // products를 가져오는 서비스 함수를 호출한다.
 
-  const products = getAllProducts();
+  const products = productsService.getAllProducts();
 
   res.status(200).json({
     status: "success",
     message: "상품 목록을 정상적으로 조회하였습니다.",
     data: products,
   });
+};
+
+export const addProduct: RequestHandler = (req, res) => {
+  try {
+    const body = validateProduct(req.body);
+
+    const product = productsService.addProduct(body);
+
+    res.status(201).json({
+      status: "success",
+      message: "상품을 정상적으로 등록하였습니다.",
+      data: product,
+    });
+  } catch (error) {
+    res.status(400).json({
+      status: "error",
+      message:
+        error instanceof Error ? error.message : "상품 등록에 실패하였습니다.",
+    });
+  }
 };
