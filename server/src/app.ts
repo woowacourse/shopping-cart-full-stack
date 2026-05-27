0b96c4fe512105a40adf1046c97ac2f172652e8b
hjkim0905 <eax9952@gmail.com>
2026-05-27 22:24:00 +0900

    refactor: 장바구니 삭제 로직 버그 해결 및 express Router 사용하여 api 도메인별 파일 분리
    Co-authored-by: lee-eojin <lejin0906@gmail.com>

--- server/src/app.ts
+++ server/src/app.ts
@@ -1,14 +1,17 @@
 import express, { Request, Response } from 'express';
+import productRouter from './routes/product';
+import cartRouter from './routes/cart';
 
 const app = express();
 
 app.use((req: Request, res: Response, next) => {
-  res.header('Access-Control-Allow-Origin', 'http://lcoalhost:3000');
+  res.header('Access-Control-Allow-Origin', 'http://localhost:3000');
   res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
   res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
   next();
 });
 
-app.listen(3000);
+app.use('/product', productRouter);
+app.use('/cart', cartRouter);
 
 export default app;
---
+++ server/src/routes/product.ts
@@ -0,0 +1,59 @@
+import express, { Request, Response } from 'express';
+import { DB } from '../database';
+import { Validator } from '../validation';
+
+const productRouter = express.Router();
+productRouter.use(express.json());
+
+// Product API
+// GET
+productRouter.get('/', (req: Request, res: Response) => {
+  if (!DB.Products) {
+    return res.status(500).json({ errorMessage: '서버에 일시적인 오류가 발생했습니다.' });
+  }
+  res.status(200).json(DB.Products);
+});
+
+// POST
+productRouter.post('/', (req: Request, res: Response) => {
+  if (!DB.Products) {
+    return res.status(500).json({ errorMessage: '서버에 일시적인 오류가 발생했습니다.' });
+  }
+
+  const { imageUrl, name, price, quantity } = req.body;
+  const newProduct = {
+    id: DB.Products.length + 1,
+    imageUrl,
+    name,
+    price,
+    quantity,
+  };
+  DB.Products.push(newProduct);
+
+  try {
+    Validator.validateRequestBody(req.body);
+    res.status(201).json({ message: '상품이 성공적으로 생성되었습니다.' });
+  } catch (error) {
+    if (error instanceof Error) {
+      res.status(400).json({ errorMessage: error.message });
+    }
+  }
+});
+
+// DELETE
+productRouter.delete('/:id', (req: Request, res: Response) => {
+  const requestedId = Number(req.params.id);
+  if (!DB.Products) {
+    return res.status(500).json({ errorMessage: '서버에 일시적인 오류가 발생했습니다.' });
+  }
+  const isIdExist = DB.Products.find((product) => product.id === requestedId);
+  if (!isIdExist) {
+    return res.status(404).send({ errorMessage: '상품을 찾을 수 없습니다.' });
+  }
+  DB.Products = DB.Products.filter((product) => product.id !== requestedId);
+  DB.Cart = DB.Cart!.filter((product) => product.id !== requestedId);
+
+  res.status(204).send();
+});
+
+export default productRouter;
