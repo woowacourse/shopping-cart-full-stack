c3890db992f444ed651e4138fa303e8a7ca22a0d
박건규 <geongyu09@gmail.com>
2026-05-28 15:23:30 +0900

    feat: 장바구니 업데이트 서비스 로직 추가
    Co-authored-by: Hyewon Jang <janghw232625@gmail.com>

--- server/src/modules/carts/carts.service.ts
+++ server/src/modules/carts/carts.service.ts
@@ -1,5 +1,30 @@
-import { getCartsQuery } from "./carts.repository";
+import ERROR_CODES from "@/ERROR_CODE";
+import { getProductByIdQuery } from "../products/products.repository";
+import {
+  getCartItemByProductIdQuery,
+  getCartsQuery,
+  updateCartQuantityQuery,
+} from "./carts.repository";
+import { validateCartQuantity } from "./carts.validator";
 
 export const getCarts = () => {
   return getCartsQuery();
+};
+
+export const changeCartQuantity = (id: number, quantity: number) => {
+  validateCartQuantity(quantity);
+
+  const existingCartItem = getCartItemByProductIdQuery(id);
+  if (!existingCartItem) throw new Error(ERROR_CODES.NOT_EXIST_CARTS_ITEM.code);
+
+  const updatedCartItem = updateCartQuantityQuery(id, quantity);
+  if (!updatedCartItem) throw new Error(ERROR_CODES.NOT_EXIST_CARTS_ITEM.code);
+
+  const product = getProductByIdQuery(id);
+  if (!product) throw new Error(ERROR_CODES.NOT_EXIST_PRODUCT.code);
+
+  return {
+    product,
+    quantity: quantity,
+  };
 };
