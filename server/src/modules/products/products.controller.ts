8da0d31ffb49f4f4e9680394c120316421a43779
stardusty-lab <startdusty.lab@gmail.com>
2026-05-28 11:00:11 +0900

    feature: createProduct 실패 케이스 controller 로직 추가
    Co-authored-by: stardusty-lab <stardusty-lab@users.noreply.github.com>
    Co-authored-by: kojesung <js95112345@gmail.com>
    Co-authored-by: 송아영 <azerosong@gmail.com>

--- server/src/modules/products/products.controller.ts
+++ server/src/modules/products/products.controller.ts
@@ -1,6 +1,7 @@
 import type { Request, Response } from "express";
 
-import { success } from "../../common/response.ts";
+import { BadRequestError } from "../../common/error.ts";
+import { fail, success } from "../../common/response.ts";
 import * as productsService from "./products.service.ts";
 
 export const getProducts = (_req: Request, res: Response) => {
@@ -11,7 +12,16 @@ export const getProducts = (_req: Request, res: Response) => {
 
 export const createProduct = (req: Request, res: Response) => {
   const newProduct = req.body;
-  const product = productsService.createProduct(newProduct);
 
-  return success(res, product);
+  try {
+    const product = productsService.createProduct(newProduct);
+
+    return success(res, product);
+  } catch (error) {
+    if (error instanceof BadRequestError) {
+      return fail(res, error.errorCode, error.errorMessage, 400, error.data);
+    }
+
+    throw error;
+  }
 };
