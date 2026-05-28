ad083859e16eb2eb342b08a4042dea31ae04bdde
stardusty-lab <startdusty.lab@gmail.com>
2026-05-28 16:23:20 +0900

    feature: cors 옵션 모두 허용
    Co-authored-by: stardusty-lab <stardusty-lab@users.noreply.github.com>
    Co-authored-by: kojesung <js95112345@gmail.com>
    Co-authored-by: 송아영 <azerosong@gmail.com>

--- server/src/app.ts
+++ server/src/app.ts
@@ -1,8 +1,12 @@
 import express from "express";
+import cors from "cors";
 
 import router from "./router/index.ts";
 
 const app = express();
+
+app.use(cors());
+
 app.use(express.json());
 
 app.get("/health", (_req, res) => {
