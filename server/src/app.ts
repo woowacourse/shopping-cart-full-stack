754bfb0d23480355557191414dd953eff93efff2
GGGGGGGGGGGGGGG_J <115770858+NaturalSoda4552@users.noreply.github.com>
2026-05-28 15:06:17 +0900

    feat: CORS 미들웨어 추가 및 설정
    Co-authored-by: TH_97 <TH-97@users.noreply.github.com>

--- server/src/app.ts
+++ server/src/app.ts
@@ -1,9 +1,17 @@
 import express from 'express';
+import cors from 'cors';
 import { errorHandler } from './middlewares/errorHandlers.js';
 import { productRouter } from './modules/products/product.routes.js';
 import { cartItemRouter } from './modules/cart/cartItem.routes.js';
 
 const app = express();
+
+app.use(
+  cors({
+    origin: ['http://localhost:3000'],
+    credentials: true,
+  }),
+);
 app.use(express.json());
 app.use(productRouter);
 app.use(cartItemRouter);
