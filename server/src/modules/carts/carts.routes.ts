6919dadd81adfaba62a669615472d1103c8fa105
박건규 <geongyu09@gmail.com>
2026-05-28 15:24:18 +0900

    feat: 장바구니 업데이트 라우트 추가
    Co-authored-by: Hyewon Jang <janghw232625@gmail.com>

--- server/src/modules/carts/carts.routes.ts
+++ server/src/modules/carts/carts.routes.ts
@@ -1,6 +1,7 @@
 import express from "express";
-import { getCarts } from "./carts.controller";
+import { getCarts, updateCartQuantity } from "./carts.controller";
 
 export const cartsRouter = express.Router();
 
 cartsRouter.get("/", getCarts);
+cartsRouter.put("/:id", updateCartQuantity);
