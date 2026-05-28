4e703793d51683e5387abcf98898677f8d2b009f
binggwa <qudrhks317@gmail.com>
2026-05-28 15:42:31 +0900

    refactor: validator에서 에러클래스 사용하도록 변경

--- server/src/repositories/util/Validator.ts
+++ server/src/repositories/util/Validator.ts
@@ -1,14 +1,15 @@
+import { InvalidError } from "../../errors/CustomErrorClass";
+import { ERROR_MESSAGE } from "../../errors/ErrorMessage";
 import { ProductInput } from "../Product";
 
 export const validateQuantity = (quantity: number): void => {
   if (quantity < 1 || quantity > 99)
-    throw new Error("quantity는 1~99 사이어야합니다.");
+    throw new InvalidError(ERROR_MESSAGE.INVALID_QUANTITY_RANGE);
 };
 
 export const validateProductData = (data: ProductInput): void => {
   validateQuantity(data.totalQuantity);
-  if (data.name.length > 100) throw new Error("name은 100자 이내여야합니다.");
-  if (data.price <= 0) throw new Error("price는 0보다 큰 숫자이어야합니다.");
-  if (!data.name) throw new Error("name은 필수 항목입니다.");
-  if (!data.thumbnailUrl) throw new Error("thumbnailUrl은 필수 항목입니다.");
+  if (data.name.length > 100 || !data.name) throw new InvalidError(ERROR_MESSAGE.INVALID_NAME);
+  if (data.price <= 0) throw new InvalidError(ERROR_MESSAGE.INVALID_PRICE);
+  if (!data.thumbnailUrl) throw new InvalidError(ERROR_MESSAGE.INVALID_THUMBNAIL_URL);
 };
