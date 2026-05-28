32aad03f9e3d849c09b01ddbc3d7cf3f38d4709d
박건규 <geongyu09@gmail.com>
2026-05-28 16:11:22 +0900

    chore: cors 환경 설정
    Co-authored-by: Hyewon Jang <janghw232625@gmail.com>

--- server/src/app.ts
+++ server/src/app.ts
@@ -4,6 +4,17 @@ import { cartsRouter } from "./modules/carts/carts.routes";
 import errorHandler from "./middlewares/errorHandler";
 
 const app = express();
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Methods", "GET,POST,PUT,DELETE,OPTIONS");
+  res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
+  if (req.method === "OPTIONS") {
+    res.sendStatus(204);
+    return;
+  }
+  next();
+});
+
 app.use(express.json());
 app.use("/products", productsRouter);
 app.use("/carts", cartsRouter);
