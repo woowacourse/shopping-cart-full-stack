d4a201be109e4bee0364aada5bd3d604c56c09b1
Antoliny0919 <antoliny0919@gmail.com>
2026-05-28 11:08:29 +0900

    feat: 문자열 길이 검증기 추가
    Co-authored-by: boboyoii <boyeon6331@gmail.com>

--- server/src/tests/Validators.test.ts
+++ server/src/tests/Validators.test.ts
@@ -1,9 +1,15 @@
-import { isNotEmpty } from "../validators.js";
+import { validateIsNotEmpty, validateLengthRange } from "../validators.js";
 
 describe("validators 테스트", () => {
   test("필수 필드를 누락되었을때 에러가 반환된다.", () => {
     expect(() => {
-      isNotEmpty("");
+      validateIsNotEmpty("");
     }).toThrow("필수 필드가 누락되었습니다.");
+  });
+
+  test("범위내 길이를 만족하지 않으면 에러가 반환된다.", () => {
+    expect(() => {
+      validateLengthRange("helloworld", 0, 5);
+    }).toThrow("0 이상 5 이하여야 합니다.");
   });
 });
--- server/src/validators.ts
+++ server/src/validators.ts
@@ -1,3 +1,9 @@
-export function isNotEmpty(value: string) {
+export function validateIsNotEmpty(value: string) {
   if (value === "") throw new Error("필수 필드가 누락되었습니다.");
+}
+
+export function validateLengthRange(value: string, min: number, max: number) {
+  if (min > value.length || value.length > max) {
+    throw new Error(`${min} 이상 ${max} 이하여야 합니다.`);
+  }
 }
