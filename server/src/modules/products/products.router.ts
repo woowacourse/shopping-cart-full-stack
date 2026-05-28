44b506cd0ce7583d63cce173b6e1b8c93ec35091
stardusty-lab <startdusty.lab@gmail.com>
2026-05-28 11:21:24 +0900

    feature: productsController 함수(조회, 추가, 삭제) router에 api로 등록
    Co-authored-by: stardusty-lab <stardusty-lab@users.noreply.github.com>
    Co-authored-by: kojesung <js95112345@gmail.com>
    Co-authored-by: 송아영 <azerosong@gmail.com>

--- server/src/modules/products/products.router.ts
+++ server/src/modules/products/products.router.ts
@@ -5,5 +5,7 @@ import * as productsController from "./products.controller.ts";
 const productsRouter = Router();
 
 productsRouter.get("/", productsController.getProducts);
+productsRouter.post("/", productsController.createProduct);
+productsRouter.delete("/:id", productsController.deleteProduct);
 
 export default productsRouter;
