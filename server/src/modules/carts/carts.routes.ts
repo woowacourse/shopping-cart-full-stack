141c3b6f14b3a5ade7a211e29a6011a66be8cc87
박건규 <geongyu09@gmail.com>
2026-05-28 15:45:28 +0900

    feat: 장바구니 상품 제거 라우트 추가
    Co-authored-by: Hyewon Jang <janghw232625@gmail.com>

--- server/src/modules/carts/carts.routes.ts
+++ server/src/modules/carts/carts.routes.ts
@@ -1,7 +1,12 @@
 import express from "express";
-import { getCarts, updateCartQuantity } from "./carts.controller";
+import {
+  deleteCartProduct,
+  getCarts,
+  updateCartQuantity,
+} from "./carts.controller";
 
 export const cartsRouter = express.Router();
 
 cartsRouter.get("/", getCarts);
 cartsRouter.put("/:id", updateCartQuantity);
+cartsRouter.delete("/:id", deleteCartProduct);
