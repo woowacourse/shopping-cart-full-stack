5e1f63791f9dce5009c5e649fc1c5aff1c4dbf1a
binggwa <qudrhks317@gmail.com>
2026-05-28 12:19:27 +0900

    feat: 커스텀에러 클래스 생성 및 에러 메시지 상수화
    Co-authored-by: yuncic <ksy151020@gmail.com>

--- server/src/errors/CustomErrorClass.ts
+++ server/src/errors/CustomErrorClass.ts
@@ -1,23 +1,19 @@
 export class NotFoundError extends Error {
-  code: string;
   status: number;
 
-  constructor(code: string, message: string) {
+  constructor(message: string) {
     super(message);
     this.name = "NotFoundError";
     this.status = 404;
-    this.code = code;
   }
 }
 
 export class InvalidError extends Error {
   status: number;
-  code: string;
 
-  constructor(code: string, message: string) {
+  constructor(message: string) {
     super(message);
     this.name = "InvalidError";
     this.status = 400;
-    this.code = code;
   }
 }
