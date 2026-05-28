72603264218bdc52e8bd9f5c9bfd92d4a613b151
goodsmell <87801306+goodsmell@users.noreply.github.com>
2026-05-28 15:44:47 +0900

    feat: cors 설정
    Co-authored-by: bel1c10ud <bel1c10ud@users.noreply.github.com>

--- server/src/app.ts
+++ server/src/app.ts
@@ -1,9 +1,13 @@
 import express from 'express';
+import cors from 'cors';
 import productRouter from './routes/productsRoute';
 import cartItemsRouter from './routes/cartItemsRoute';
 import errorHandler from './middlewares/errorHandler';
 
 const app = express();
+
+app.use(cors());
+
 app.use(express.json());
 
 app.use('', productRouter);
