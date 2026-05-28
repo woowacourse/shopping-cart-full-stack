6b963b06fa31d919df9ee0be1fab43cbfd187e36
Antoliny0919 <antoliny0919@gmail.com>
2026-05-28 11:24:40 +0900

    feat: 검증기에 라벨이 전달되도록 확장
    Co-authored-by: boboyoii <boyeon6331@gmail.com>

--- server/src/tests/Validators.test.ts
+++ server/src/tests/Validators.test.ts
@@ -6,20 +6,23 @@ import {
 
 describe("validators 테스트", () => {
   test("필수 필드를 누락되었을때 에러가 반환된다.", () => {
+    const validator = validateIsNotEmpty("상품명");
     expect(() => {
-      validateIsNotEmpty("");
-    }).toThrow("필수 필드가 누락되었습니다.");
+      validator("");
+    }).toThrow("상품명 필드가 누락되었습니다.");
   });
 
   test("범위내 길이를 만족하지 않으면 에러가 반환된다.", () => {
+    const validator = validateLengthRange("상품명");
     expect(() => {
-      validateLengthRange("helloworld", 0, 5);
-    }).toThrow("0 이상 5 이하여야 합니다.");
+      validator("helloworld", 0, 5);
+    }).toThrow("상품명은 0 이상 5 이하여야 합니다.");
   });
 
   test("최소숫자보다 낮으면 에러가 반환된다.", () => {
+    const validator = validateMinNumber("가격");
     expect(() => {
-      validateMinNumber(10000, 20000);
-    }).toThrow("20000 보다 큰 숫자여야 합니다.");
+      validator(10000, 20000);
+    }).toThrow("가격은 20000 보다 큰 숫자여야 합니다.");
   });
 });
--- server/src/validators.ts
+++ server/src/validators.ts
@@ -1,15 +1,21 @@
-export function validateIsNotEmpty(value: string) {
-  if (value === "") throw new Error("필수 필드가 누락되었습니다.");
+export function validateIsNotEmpty(label: string) {
+  return function validate(value: string) {
+    if (value === "") throw new Error(`${label} 필드가 누락되었습니다.`);
+  };
 }
 
-export function validateLengthRange(value: string, min: number, max: number) {
-  if (min > value.length || value.length > max) {
-    throw new Error(`${min} 이상 ${max} 이하여야 합니다.`);
-  }
+export function validateLengthRange(label: string) {
+  return function validate(value: string, min: number, max: number) {
+    if (min > value.length || value.length > max) {
+      throw new Error(`${label}은 ${min} 이상 ${max} 이하여야 합니다.`);
+    }
+  };
 }
 
-export function validateMinNumber(value: number, min: number) {
-  if (value <= min) {
-    throw new Error(`${min} 보다 큰 숫자여야 합니다.`);
-  }
+export function validateMinNumber(label: string) {
+  return function validate(value: number, min: number) {
+    if (value <= min) {
+      throw new Error(`${label}은 ${min} 보다 큰 숫자여야 합니다.`);
+    }
+  };
 }
