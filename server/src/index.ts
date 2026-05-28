7ac036a248e7c33231571afff0bfbc592c263784
Antoliny0919 <antoliny0919@gmail.com>
2026-05-28 11:59:44 +0900

    fix: 삭제된 핸들러 정리
    Co-authored-by: boboyoii <boyeon6331@gmail.com>

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,11 +1,9 @@
 import InMemoryStorage from "./storages/InMemoryStorage.js";
-import StorageHandler from "./StorageHandler.js";
 import { createApp } from "./app.js";
 
 const PORT = process.env.PORT ?? 3000;
 
-const storageHandler = new StorageHandler(new InMemoryStorage());
-const app = createApp<InMemoryStorage>(storageHandler);
+const app = createApp(new InMemoryStorage());
 
 app.listen(PORT, () => {
   console.log(`Server listening on port ${PORT}`);
