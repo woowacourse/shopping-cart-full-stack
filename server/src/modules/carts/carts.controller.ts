018229e1f8bc616b3d28fa3de983cd9f608c7f27
stardusty-lab <startdusty.lab@gmail.com>
2026-05-28 15:50:41 +0900

    feature: carts api 호출 레이어 구현
    Co-authored-by: stardusty-lab <stardusty-lab@users.noreply.github.com>
    Co-authored-by: kojesung <js95112345@gmail.com>
    Co-authored-by: 송아영 <azerosong@gmail.com>

---
+++ server/src/modules/carts/carts.controller.ts
@@ -0,0 +1,67 @@
+import type { Request, Response } from "express";
+
+import { ServiceError } from "../../common/error.ts";
+import { fail, success } from "../../common/response.ts";
+import * as cartsService from "./carts.service.ts";
+
+const getStatusCode = (error: ServiceError<unknown>) => {
+  if (error.errorCode === "RESOURCE_NOT_FOUND") return 404;
+
+  return 400;
+};
+
+export const getCartById = (req: Request, res: Response) => {
+  const cartId = Number(req.params.cartId);
+
+  try {
+    const cart = cartsService.getCartById(cartId);
+
+    return success(res, cart);
+  } catch (error) {
+    if (error instanceof ServiceError) {
+      return fail(res, error.errorCode, error.errorMessage, 404, error.data);
+    }
+
+    throw error;
+  }
+};
+
+export const updateCartProduct = (req: Request, res: Response) => {
+  const cartId = Number(req.params.cartId);
+  const productId = Number(req.params.productId);
+
+  try {
+    const product = cartsService.updateCartProduct(cartId, productId, req.body);
+
+    return success(res, product);
+  } catch (error) {
+    if (error instanceof ServiceError) {
+      return fail(
+        res,
+        error.errorCode,
+        error.errorMessage,
+        getStatusCode(error),
+        error.data,
+      );
+    }
+
+    throw error;
+  }
+};
+
+export const deleteCartProduct = (req: Request, res: Response) => {
+  const cartId = Number(req.params.cartId);
+  const productId = Number(req.params.productId);
+
+  try {
+    cartsService.deleteCartProduct(cartId, productId);
+
+    return res.status(204).send();
+  } catch (error) {
+    if (error instanceof ServiceError) {
+      return fail(res, error.errorCode, error.errorMessage, 404, error.data);
+    }
+
+    throw error;
+  }
+};
