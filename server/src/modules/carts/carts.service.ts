a248b1e9511507587319905fd51d5f0102f9c88e
stardusty-lab <startdusty.lab@gmail.com>
2026-05-28 14:23:09 +0900

    feature: getCartById 존재하지 않는 id에 대한 기능 구현
    Co-authored-by: stardusty-lab <stardusty-lab@users.noreply.github.com>
    Co-authored-by: kojesung <js95112345@gmail.com>
    Co-authored-by: 송아영 <azerosong@gmail.com>

--- server/src/modules/carts/carts.service.ts
+++ server/src/modules/carts/carts.service.ts
@@ -1,10 +1,17 @@
 import { Cart, ProductInCart } from "./carts.model.ts";
 import * as cartsRepository from "./carts.repository.ts";
 import * as productsRepository from "../products/products.repository.ts";
+import { ServiceError } from "../../common/error.ts";
 
 export const getCartById = (cartId: number) => {
   const cart = cartsRepository.findById(cartId);
-  if (!cart) return undefined;
+  if (!cart)
+    throw new ServiceError(
+      "RESOURCE_NOT_FOUND",
+      "id에 해당하는 장바구니가 존재하지 않습니다.",
+      404,
+    );
+
   return {
     id: cart.id,
     products: cart.products.map((product) => ({
