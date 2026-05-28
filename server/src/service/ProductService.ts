ef02c85a7716c3710d24f1c96029b5991e090668
binggwa <qudrhks317@gmail.com>
2026-05-28 13:34:55 +0900

    refactor: ProductController에서 repository 접근 로직 리팩토링
    Co-authored-by: yuncic <ksy151020@gmail.com>

---
+++ server/src/service/ProductService.ts
@@ -0,0 +1,26 @@
+import { InvalidError, NotFoundError } from "../errors/CustomErrorClass";
+import { ERROR_MESSAGE } from "../errors/ErrorMessage";
+import { cartRepository } from "../repositories/CartRepository";
+import { ProductData } from "../repositories/Product";
+import { productRepository } from "../repositories/ProductRepository";
+
+export const getProductsService = (): ProductData[] => {
+  return productRepository.getProducts();
+};
+
+export const postProductsService = (newProducts: ProductData): ProductData => {
+  const addedProduct = productRepository.addProduct(newProducts);
+  if (!addedProduct) throw new NotFoundError(ERROR_MESSAGE.NOT_FOUND_PRODUCT);
+
+  return addedProduct;
+};
+
+export const deleteProductsService = (productId: number): void => {
+  if (!productId) throw new InvalidError(ERROR_MESSAGE.INVALID_PRODUCT_ID);
+
+  const product = productRepository.findById(productId);
+  if (!product) throw new NotFoundError(ERROR_MESSAGE.NOT_FOUND_PRODUCT);
+
+  productRepository.deleteById(productId);
+  cartRepository.deleteByProductId(productId);
+};
