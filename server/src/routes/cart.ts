b8752f20b8598b81df6fb4912dcc688a4f89ac95
lee-eojin <lejin0906@snu.ac.kr>
2026-05-31 01:30:13 +0900

    refactor: 모듈 DB import를 제거하고, 주입받은 db를 쓰도록 변경

--- server/src/routes/cart.ts
+++ server/src/routes/cart.ts
@@ -1,71 +1,63 @@
 import express, { Request, Response } from 'express';
-import { DB } from '../database';
+import { Database } from '../database';
 import { Validator } from '../validation';
 
-const cartRouter = express.Router();
-cartRouter.use(express.json());
+export function createCartRouter(db: Database) {
+  const cartRouter = express.Router();
+  cartRouter.use(express.json());
 
-// Cart API
-// GET
-cartRouter.get('/', (req: Request, res: Response) => {
-  if (!DB.Cart) {
-    return res.status(500).json({ errorMessage: '서버에 일시적인 오류가 발생했습니다.' });
-  }
-  res.status(200).json(DB.Cart);
-});
-
-// POST
-cartRouter.post('/:id', function (req: Request, res: Response) {
-  const requestId = Number(req.params.id);
-
-  if (!DB.Cart) {
-    return res.status(500).json({ errorMessage: '서버에 일시적인 오류가 발생했습니다.' });
-  }
-  const pickedProduct = DB.Products!.find((product) => product.id === requestId);
-  if (!pickedProduct) {
-    return res.status(404).json({ errorMessage: '상품을 찾을 수 없습니다.' });
-  }
-  DB.Cart.push(pickedProduct);
-  res.status(201).json({ message: '상품이 장바구니에 추가되었습니다.' });
-});
-
-// PUT
-cartRouter.put('/', function (req: Request, res: Response) {
-  const { id } = req.body;
-
-  if (!DB.Cart) {
-    return res.status(500).json({ errorMessage: '서버에 일시적인 오류가 발생했습니다.' });
-  }
-
-  const toBeUpdatedIndex = DB.Cart.findIndex((product) => product.id === id);
-  if (toBeUpdatedIndex === -1) {
-    return res.status(404).json({ errorMessage: '상품을 찾을 수 없습니다.' });
-  }
-
-  try {
-    Validator.validateRequestBody(req.body);
-    DB.Cart[toBeUpdatedIndex] = req.body;
-    res.status(204).send();
-  } catch (error) {
-    if (error instanceof Error) {
-      res.status(400).json({ errorMessage: error.message });
+  cartRouter.get('/', (req: Request, res: Response) => {
+    if (!db.Cart) {
+      return res.status(500).json({ errorMessage: '서버에 일시적인 오류가 발생했습니다.' });
     }
-  }
-});
+    res.status(200).json(db.Cart);
+  });
 
-// DELETE
-cartRouter.delete('/:id', (req: Request, res: Response) => {
-  const requestId = Number(req.params.id);
-  if (!DB.Cart) {
-    return res.status(500).json({ errorMessage: '서버에 일시적인 오류가 발생했습니다.' });
-  }
-  const isIdExist = DB.Cart.find((product) => product.id === requestId);
-  if (!isIdExist) {
-    return res.status(404).send({ errorMessage: '상품을 찾을 수 없습니다.' });
-  }
-  DB.Cart = DB.Cart.filter((product) => product.id !== requestId);
+  cartRouter.post('/:id', function (req: Request, res: Response) {
+    const requestId = Number(req.params.id);
+    if (!db.Cart) {
+      return res.status(500).json({ errorMessage: '서버에 일시적인 오류가 발생했습니다.' });
+    }
+    const pickedProduct = db.Products!.find((product) => product.id === requestId);
+    if (!pickedProduct) {
+      return res.status(404).json({ errorMessage: '상품을 찾을 수 없습니다.' });
+    }
+    db.Cart.push(pickedProduct);
+    res.status(201).json({ message: '상품이 장바구니에 추가되었습니다.' });
+  });
 
-  res.status(204).send();
-});
+  cartRouter.put('/', function (req: Request, res: Response) {
+    const { id } = req.body;
+    if (!db.Cart) {
+      return res.status(500).json({ errorMessage: '서버에 일시적인 오류가 발생했습니다.' });
+    }
+    const toBeUpdatedIndex = db.Cart.findIndex((product) => product.id === id);
+    if (toBeUpdatedIndex === -1) {
+      return res.status(404).json({ errorMessage: '상품을 찾을 수 없습니다.' });
+    }
+    try {
+      Validator.validateRequestBody(req.body);
+      db.Cart[toBeUpdatedIndex] = req.body;
+      res.status(204).send();
+    } catch (error) {
+      if (error instanceof Error) {
+        res.status(400).json({ errorMessage: error.message });
+      }
+    }
+  });
 
-export default cartRouter;
+  cartRouter.delete('/:id', (req: Request, res: Response) => {
+    const requestId = Number(req.params.id);
+    if (!db.Cart) {
+      return res.status(500).json({ errorMessage: '서버에 일시적인 오류가 발생했습니다.' });
+    }
+    const isIdExist = db.Cart.find((product) => product.id === requestId);
+    if (!isIdExist) {
+      return res.status(404).send({ errorMessage: '상품을 찾을 수 없습니다.' });
+    }
+    db.Cart = db.Cart.filter((product) => product.id !== requestId);
+    res.status(204).send();
+  });
+
+  return cartRouter;
+}
