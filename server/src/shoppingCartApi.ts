eaf671ed53e5dc350e48490789e44baa89e46036
JuHyeong424 <wkdwngud963@gmail.com>
2026-05-30 11:26:05 +0900

    refactor: 상품 삭제를 장바구니 상품 삭제 함수로 수정

--- server/src/shoppingCartApi.ts
+++ server/src/shoppingCartApi.ts
@@ -1,16 +1,15 @@
-import { Router } from 'express';
-
-import { deleteProduct } from './service/productService.ts';
+import { Router } from "express";
+import { deleteShoppingCart } from "./service/shoppingCartService.ts";
 import {
   getShoppingCart,
   patchShoppingCart,
-} from './service/shoppingCartService.ts';
+} from "./service/shoppingCartService.ts";
 
-import { shoppingCart } from './database/inMemoryDatabase.ts';
+import { shoppingCart } from "./database/inMemoryDatabase.ts";
 
 const router = Router();
 
-router.get('/', (_req, res, next) => {
+router.get("/", (_req, res, next) => {
   try {
     res.status(200).send(getShoppingCart());
   } catch (error) {
@@ -18,24 +17,24 @@ router.get('/', (_req, res, next) => {
   }
 });
 
-router.patch('/:id', (req, res, next) => {
+router.patch("/:id", (req, res, next) => {
   try {
     const productId = req.params.id;
     const request = req.body.quantity;
     if (!shoppingCart.hasProductId(productId)) {
-      return res.status(404).send({ message: '유효하지 않은 경로입니다.' });
+      return res.status(404).send({ message: "유효하지 않은 경로입니다." });
     }
 
     if (req.body.quantity < 1) {
       return res
         .status(400)
-        .send({ message: '상품 수량이 1 이상이어야 합니다.' });
+        .send({ message: "상품 수량이 1 이상이어야 합니다." });
     }
 
     if (req.body.quantity > 99) {
       return res
         .status(400)
-        .send({ message: '상품 수량이 99 이하여야 합니다.' });
+        .send({ message: "상품 수량이 99 이하여야 합니다." });
     }
 
     patchShoppingCart(productId, request);
@@ -45,22 +44,22 @@ router.patch('/:id', (req, res, next) => {
   }
 });
 
-router.delete('/:id', (req, res, next) => {
+router.delete("/:id", (req, res, next) => {
   try {
     const productId = req.params.id;
     if (!shoppingCart.hasProductId(productId)) {
-      return res.status(404).send({ message: '유효하지 않은 경로입니다.' });
+      return res.status(404).send({ message: "유효하지 않은 경로입니다." });
     }
-    deleteProduct(productId);
+    deleteShoppingCart(productId);
     res.status(204).send();
   } catch (error) {
     next(error);
   }
 });
 
-router.post('/', (_req, res) => {
+router.post("/", (_req, res) => {
   res.status(501).send({
-    message: 'Not Implemented',
+    message: "Not Implemented",
   });
 });
 
