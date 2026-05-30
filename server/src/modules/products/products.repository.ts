237a2031b656057425215a44bc79bc111974b783
kojesung <js95112345@gmail.com>
2026-05-30 16:33:51 +0900

    refactor: id 생성 기준 최대값 +1로 변경 및 ProductRepository 인터페이스 생성

--- server/src/modules/products/products.repository.ts
+++ server/src/modules/products/products.repository.ts
@@ -2,32 +2,40 @@ import { Product } from './products.model.ts';
 import { rawProducts } from '../../raw/raw.products.ts';
 import type { ProductRequest } from './products.dto.ts';
 
-export const findAll = () => {
-    return rawProducts.map((product) => new Product(product));
-};
+export interface ProductsRepository {
+    findAll(): Product[];
+    findById(id: number): Product | undefined;
+    create(product: ProductRequest): Product;
+    deleteById(id: number): void;
+}
 
-export const findById = (id: number) => {
-    const product = rawProducts.find((product) => product.id === id);
+class InMemoryProductsRepository implements ProductsRepository {
+    findAll() {
+        return rawProducts.map((product) => new Product(product));
+    }
 
-    return product ? new Product(product) : undefined;
-};
+    findById(id: number) {
+        const product = rawProducts.find((product) => product.id === id);
 
-export const create = (product: ProductRequest) => {
-    const id = (rawProducts.at(-1)?.id ?? 0) + 1;
-    const newProduct = {
-        ...product,
-        id,
-    };
+        return product ? new Product(product) : undefined;
+    }
 
-    rawProducts.push(newProduct);
+    create(product: ProductRequest) {
+        const id = Math.max(0, ...rawProducts.map((p) => p.id)) + 1;
+        const newProduct = { ...product, id };
 
-    return newProduct;
-};
+        rawProducts.push(newProduct);
 
-export const deleteById = (id: number) => {
-    const productIndex = rawProducts.findIndex((product) => product.id === id);
+        return new Product(newProduct);
+    }
 
-    if (productIndex === -1) return;
+    deleteById(id: number) {
+        const productIndex = rawProducts.findIndex((product) => product.id === id);
 
-    rawProducts.splice(productIndex, 1);
-};
+        if (productIndex === -1) return;
+
+        rawProducts.splice(productIndex, 1);
+    }
+}
+
+export const productsRepository: ProductsRepository = new InMemoryProductsRepository();
