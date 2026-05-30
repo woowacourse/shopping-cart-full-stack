66d2ad6f5211e705e9a5ff40b8ca85e962ceac0e
Antoliny0919 <antoliny0919@gmail.com>
2026-05-31 00:23:26 +0900

    refactor: 동일한 바디 검증 미들웨어를 생성하는 팩토리 함수 추가

--- server/src/middlewares/BodyValiadateMiddleware.ts
+++ server/src/middlewares/BodyValiadateMiddleware.ts
@@ -1,41 +1,28 @@
 import { Request, Response, NextFunction } from "express";
 import { ProductFieldValidators, CartFieldValidators } from "../validators.js";
+import { ValidatorMap } from "../utils.js";
 import { runValidate } from "../utils.js";
 
-export function productBodyValidateMiddelware(
-  req: Request,
-  res: Response,
-  next: NextFunction,
-) {
-  const errors = runValidate(ProductFieldValidators, req.body);
+function createValidateBodyMiddleware(validatorMap: ValidatorMap) {
+  return function middleware(req: Request, res: Response, next: NextFunction) {
+    const errors = runValidate(validatorMap, req.body);
 
-  const hasErrors = Object.keys(errors).length !== 0;
-  if (hasErrors) {
-    res.status(400).send({
-      code: "INVALID_BODY",
-      message: "요청 데이터가 유효하지 않습니다.",
-      errors: errors,
-    });
-    return;
-  }
-  next();
+    const hasErrors = Object.keys(errors).length > 0;
+    if (hasErrors) {
+      res.status(400).send({
+        code: "INVALID_BODY",
+        message: "요청 데이터가 유효하지 않습니다.",
+        errors: errors,
+      });
+      return;
+    }
+    next();
+  };
 }
 
-export function cartBodyValidateMiddelware(
-  req: Request<{ id: string }>,
-  res: Response,
-  next: NextFunction,
-) {
-  const errors = runValidate(CartFieldValidators, req.body);
+export const productBodyValidateMiddelware = createValidateBodyMiddleware(
+  ProductFieldValidators,
+);
 
-  const hasErrors = Object.keys(errors).length !== 0;
-  if (hasErrors) {
-    res.status(400).send({
-      code: "INVALID_BODY",
-      message: "요청 데이터가 유효하지 않습니다.",
-      errors: errors,
-    });
-    return;
-  }
-  next();
-}
+export const cartBodyValidateMiddelware =
+  createValidateBodyMiddleware(CartFieldValidators);
