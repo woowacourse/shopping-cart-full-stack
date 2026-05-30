b40fd705f454afa98c35c74b3618467780ae2ee9
hjkim0905 <eax9952@gmail.com>
2026-05-30 20:58:46 +0900

    refactor: OPTIONS preflight를 처리하기 위한 분기 추가

--- server/src/app.ts
+++ server/src/app.ts
@@ -8,6 +8,9 @@ app.use((req: Request, res: Response, next) => {
   res.header('Access-Control-Allow-Origin', 'http://localhost:3000');
   res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
   res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+  if (req.method === 'OPTIONS') {
+    return res.sendStatus(204);
+  }
   next();
 });
 
