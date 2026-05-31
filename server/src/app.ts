2b50fd22b3099428a902fef77c9b7d0ff66f9358
janghw0126 <janghw232625@gmail.com>
2026-05-31 16:32:26 +0900

    refactor: CORS 미들웨어를 별도의 파일로 분리하여 코드 구조 개선

--- server/src/app.ts
+++ server/src/app.ts
@@ -2,19 +2,11 @@ import express from "express";
 import { productsRouter } from "./modules/products/products.routes";
 import { cartsRouter } from "./modules/carts/carts.routes";
 import errorHandler from "./middlewares/errorHandler";
+import cors from "./middlewares/cors";
 
 const app = express();
-app.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Methods", "GET,POST,PUT,DELETE,OPTIONS");
-  res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
-  if (req.method === "OPTIONS") {
-    res.sendStatus(204);
-    return;
-  }
-  next();
-});
 
+app.use(cors);
 app.use(express.json());
 app.use("/products", productsRouter);
 app.use("/carts", cartsRouter);
