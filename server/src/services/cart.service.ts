82d45e9dd3f6bd8dc41a7753e5d253400ae4c902
yundoll <mar0722@naver.com>
2026-05-31 22:40:16 +0900

    refactor: 상품 서비스의 장바구니 레포지토리 의존 제거

--- server/src/services/cart.service.ts
+++ server/src/services/cart.service.ts
@@ -5,8 +5,9 @@ import {
   deleteById,
   findProductIdById,
   updateItemQuantity,
+  deleteByProductId,
 } from "../repositories/cart.repository.js";
-import { getStockByID } from "./products.service.js";
+import { getProductStockById } from "./products.service.js";
 
 export async function getCartItems() {
   return await findAll();
@@ -16,7 +17,7 @@ export async function updateCartItemQuantity(id: number, quantity: number) {
   const productId = findProductIdById(id);
   if (productId === null) throw new Error(CART_ERROR_RESPONSE.CART_ITEM_NOT_FOUND.code);
 
-  const stock = await getStockByID(productId);
+  const stock = await getProductStockById(productId);
   if (stock === null) throw new Error(PRODUCT_ERROR_RESPONSE.PRODUCT_NOT_FOUND.code);
   if (quantity > stock) throw new Error(CART_ERROR_RESPONSE.OUT_OF_STOCK.code);
 
@@ -28,4 +29,8 @@ export async function deleteCartItem(id: number) {
     throw new Error(CART_ERROR_RESPONSE.CART_ITEM_NOT_FOUND.code);
   }
   await deleteById(id);
+}
+
+export async function deleteCartItemByProductId(prductId: number) {
+  await deleteByProductId(prductId);
 }
--- server/src/services/products.service.ts
+++ server/src/services/products.service.ts
@@ -1,7 +1,7 @@
 import { CreateProductDto } from "../interfaces/product.interface.js";
 import { save, findAll, deleteById, isAlreadyExist, findStockById } from "../repositories/products.repository.js";
-import { deleteByProductId } from "../repositories/cart.repository.js";
 import { PRODUCT_ERROR_RESPONSE } from "../constants/error.js";
+import { deleteCartItemByProductId } from "./cart.service.js";
 
 export async function addProduct(product: CreateProductDto) {
   await save(product);
@@ -11,7 +11,7 @@ export async function getProducts() {
   return await findAll();
 }
 
-export async function getStockByID(id: number) {
+export async function getProductStockById(id: number) {
   return await findStockById(id);
 }
 
@@ -20,5 +20,5 @@ export async function deleteProduct(id: number) {
     throw new Error(PRODUCT_ERROR_RESPONSE.PRODUCT_NOT_FOUND.code);
   }
   await deleteById(id);
-  await deleteByProductId(id);
+  await deleteCartItemByProductId(id);
 }
