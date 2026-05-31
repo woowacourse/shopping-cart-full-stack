907c6070ffe506d9d5b552aa02899f9bf0055703
lee-eojin <lejin0906@snu.ac.kr>
2026-05-31 13:06:54 +0900

    refactor: 실제 db를 주입하도록 변경

--- server/src/app.ts
+++ server/src/app.ts
@@ -1,17 +1,18 @@
-import express, { Request, Response } from 'express';
-import productRouter from './routes/product';
-import cartRouter from './routes/cart';
+import express, { Request, Response } from "express";
+import { createProductRouter } from "./routes/product";
+import { createCartRouter } from "./routes/cart";
+import { DB } from "./database";
 
 const app = express();
 
 app.use((req: Request, res: Response, next) => {
-  res.header('Access-Control-Allow-Origin', 'http://localhost:3000');
-  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
-  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+  res.header("Access-Control-Allow-Origin", "http://localhost:3000");
+  res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
   next();
 });
 
-app.use('/product', productRouter);
-app.use('/cart', cartRouter);
+app.use("/product", createProductRouter(DB));
+app.use("/cart", createCartRouter(DB));
 
 export default app;
