8bade931ca2ca2444365cf78a04a953cd54ce67e
김예안 <kjan0929@naver.com>
2026-06-02 00:25:47 +0900

    feat: InMemoryProductRepository 구현

--- server/src/domain/product/product.repository.ts
+++ server/src/domain/product/product.repository.ts
@@ -1,17 +1,23 @@
-import { ProductType } from '../../model/Product.js';
+import { products } from '../../db/inMemoryDb.js';
+import Product from '../../model/Product.js';
 
 export interface ProductRepository {
   get: () => void;
-  add: ({ name, price, quantity, imgUrl }: Omit<ProductType, 'id'>) => void;
+  add: (product: Product) => void;
   delete: (id: number) => void;
 }
 
 export class InMemoryProductRepository implements ProductRepository {
-  constructor() {}
+  get() {
+    return [...products];
+  }
 
-  get() {}
+  add(product: Product) {
+    products.push(product);
+  }
 
-  add() {}
-
-  delete() {}
+  delete(id: number) {
+    const index = products.findIndex((p) => p.toJson().id === id);
+    if (index !== -1) products.splice(index, 1);
+  }
 }
