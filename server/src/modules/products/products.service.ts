e34b95968a4828fa35df898301485a2674f6cb5e
stardusty-lab <startdusty.lab@gmail.com>
2026-06-01 17:22:43 +0900

    refactor: createProduct 유효성 검증 함수 외부 함수로 export
    Co-authored-by: stardusty-lab <stardusty-lab@users.noreply.github.com>
    Co-authored-by: kojesung <js95112345@gmail.com>
    Co-authored-by: 송아영 <azerosong@gmail.com>

--- server/src/modules/products/products.service.ts
+++ server/src/modules/products/products.service.ts
@@ -3,7 +3,7 @@ import * as productsRepository from "./products.repository.ts";
 import * as cartsRepository from "../carts/carts.repository.ts";
 import { ServiceError } from "../../common/error.ts";
 import type { ProductRequest } from "./products.dto.ts";
-import { getMissingFields } from "../../validate/getMissingFields.ts";
+import * as validate from "./products.validate.ts";
 
 export const getProducts = (): ProductResponse[] => {
   return productsRepository.findAll().map((product) => ({
@@ -15,57 +15,9 @@ export const getProducts = (): ProductResponse[] => {
 };
 
 export const createProduct = (productRequest: Partial<ProductRequest>) => {
-  // 필수값 검증
-  const requiredFields = ["price", "name", "imgUrl"] as const;
-  const missingFields = getMissingFields(
-    productRequest,
-    requiredFields as unknown as string[],
-  );
-
-  if (missingFields.length > 0) {
-    throw new ServiceError(
-      "MISSING_FIELD",
-      "필수값이 누락되었습니다.",
-      missingFields.map((field) => ({
-        type: field,
-        errorCode: `MISSING_FIELD_${field.toUpperCase()}`,
-      })),
-    );
-  }
-
-  // 타입 검증
-  const isTypeMismatch = requiredFields.some((field) => {
-    if (field === "price") return typeof productRequest[field] !== "number";
-
-    return typeof productRequest[field] !== "string";
-  });
-
-  if (isTypeMismatch) {
-    throw new ServiceError("TYPE_MISMATCH", "타입이 일치하지 않습니다.");
-  }
-
-  // 도메인 규칙 검증
-  const invalidFields = [
-    {
-      type: "price",
-      isInvalid: productRequest.price! <= 0,
-    },
-    {
-      type: "name",
-      isInvalid: productRequest.name!.length > 100,
-    },
-  ].filter(({ isInvalid }) => isInvalid);
-
-  if (invalidFields.length > 0) {
-    throw new ServiceError(
-      "INVALID",
-      "도메인 규칙에 맞지 않는 값입니다.",
-      invalidFields.map(({ type }) => ({
-        type,
-        errorCode: `INVALID_${type.toUpperCase()}`,
-      })),
-    );
-  }
+  validate.createProduct.validateRequiredFields(productRequest);
+  validate.createProduct.validateTypes(productRequest);
+  validate.createProduct.validateDomainRules(productRequest);
 
   const product = productsRepository.create(productRequest as ProductRequest);
 
