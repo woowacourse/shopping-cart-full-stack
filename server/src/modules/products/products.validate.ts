83ac0c4fa32ff54e7fac065641fdd6cf4f2eda79
stardusty-lab <startdusty.lab@gmail.com>
2026-06-01 17:51:47 +0900

    refactor: 개별 유효성 검증 함수로 외부 선언
    Co-authored-by: stardusty-lab <stardusty-lab@users.noreply.github.com>
    Co-authored-by: kojesung <js95112345@gmail.com>
    Co-authored-by: 송아영 <azerosong@gmail.com>

--- server/src/modules/products/products.validate.ts
+++ server/src/modules/products/products.validate.ts
@@ -3,67 +3,72 @@ import { getMissingFields } from "../../validate/getMissingFields.ts";
 
 import type { ProductRequest } from "./products.dto.ts";
 
+// createProduct
+const validateCreateProductRequiredFields = (
+  productRequest: Partial<ProductRequest>,
+) => {
+  // 필수값 검증
+  const requiredFields = ["price", "name", "imgUrl"] as const;
+  const missingFields = getMissingFields(
+    productRequest,
+    requiredFields as unknown as string[],
+  );
+
+  if (missingFields.length > 0) {
+    throw new ServiceError(
+      "MISSING_FIELD",
+      "필수값이 누락되었습니다.",
+      missingFields.map((field) => ({
+        type: field,
+        errorCode: `MISSING_FIELD_${field.toUpperCase()}`,
+      })),
+    );
+  }
+};
+const validateCreateProductTypes = (
+  productRequest: Partial<ProductRequest>,
+) => {
+  // 타입 검증
+  const requiredFields = ["price", "name", "imgUrl"] as const;
+  const isTypeMismatch = requiredFields.some((field) => {
+    if (field === "price") return typeof productRequest[field] !== "number";
+
+    return typeof productRequest[field] !== "string";
+  });
+
+  if (isTypeMismatch) {
+    throw new ServiceError("TYPE_MISMATCH", "타입이 일치하지 않습니다.");
+  }
+};
+const validateCreateProductDomainRules = (
+  productRequest: Partial<ProductRequest>,
+) => {
+  // 도메인 규칙 검증
+  const invalidFields = [
+    {
+      type: "price",
+      isInvalid: productRequest.price! <= 0,
+    },
+    {
+      type: "name",
+      isInvalid: productRequest.name!.length > 100,
+    },
+  ].filter(({ isInvalid }) => isInvalid);
+
+  if (invalidFields.length > 0) {
+    throw new ServiceError(
+      "INVALID",
+      "도메인 규칙에 맞지 않는 값입니다.",
+      invalidFields.map(({ type }) => ({
+        type,
+        errorCode: `INVALID_${type.toUpperCase()}`,
+      })),
+    );
+  }
+};
+
 export const createProduct = (productRequest: Partial<ProductRequest>) => {
-  const validate = {
-    validateRequiredFields(productRequest: Partial<ProductRequest>) {
-      // 필수값 검증
-      const requiredFields = ["price", "name", "imgUrl"] as const;
-      const missingFields = getMissingFields(
-        productRequest,
-        requiredFields as unknown as string[],
-      );
-
-      if (missingFields.length > 0) {
-        throw new ServiceError(
-          "MISSING_FIELD",
-          "필수값이 누락되었습니다.",
-          missingFields.map((field) => ({
-            type: field,
-            errorCode: `MISSING_FIELD_${field.toUpperCase()}`,
-          })),
-        );
-      }
-    },
-    validateTypes(productRequest: Partial<ProductRequest>) {
-      // 타입 검증
-      const requiredFields = ["price", "name", "imgUrl"] as const;
-      const isTypeMismatch = requiredFields.some((field) => {
-        if (field === "price") return typeof productRequest[field] !== "number";
-
-        return typeof productRequest[field] !== "string";
-      });
-
-      if (isTypeMismatch) {
-        throw new ServiceError("TYPE_MISMATCH", "타입이 일치하지 않습니다.");
-      }
-    },
-    validateDomainRules(productRequest: Partial<ProductRequest>) {
-      // 도메인 규칙 검증
-      const invalidFields = [
-        {
-          type: "price",
-          isInvalid: productRequest.price! <= 0,
-        },
-        {
-          type: "name",
-          isInvalid: productRequest.name!.length > 100,
-        },
-      ].filter(({ isInvalid }) => isInvalid);
-
-      if (invalidFields.length > 0) {
-        throw new ServiceError(
-          "INVALID",
-          "도메인 규칙에 맞지 않는 값입니다.",
-          invalidFields.map(({ type }) => ({
-            type,
-            errorCode: `INVALID_${type.toUpperCase()}`,
-          })),
-        );
-      }
-    },
-  };
-
-  validate.validateRequiredFields(productRequest);
-  validate.validateTypes(productRequest);
-  validate.validateDomainRules(productRequest);
+  validateCreateProductRequiredFields(productRequest);
+  validateCreateProductTypes(productRequest);
+  validateCreateProductDomainRules(productRequest);
 };
