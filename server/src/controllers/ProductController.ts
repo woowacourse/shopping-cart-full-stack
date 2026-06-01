30fa81d517cc6e77d212a99780ee0bbc4dba85fb
eastar-DS <dororo0104@naver.com>
2026-06-01 09:22:10 +0900

    refactor(ProductController): 컨트롤러에 Happy path만 남기고 res.body.body로 접근하던걸 제거하기위해 body 속성 제거

--- server/src/controllers/ProductController.ts
+++ server/src/controllers/ProductController.ts
@@ -1,42 +1,20 @@
-import type {Request, Response} from 'express';
-
-import {productService} from '../services/ProductService.js';
-import type {CreateProductRequestBody, IdParams} from '../type.js';
+import type { Request, Response } from "express";
+import { productService } from "../services/ProductService.js";
+import type { IdParams } from "../type.js";
 
 export const productController = {
   getProducts(_req: Request, res: Response) {
-    res.status(200).json({
-      body: productService.getProducts(),
-    });
+    res.status(200).json(productService.getProducts());
   },
 
-  createProduct(req: Request<{}, unknown, CreateProductRequestBody>, res: Response) {
-    const result = productService.createProduct(req.body);
+  createProduct(req: Request, res: Response) {
+    const id = productService.createProduct(req.body);
 
-    if (result.status === 'invalid') {
-      return res.status(400).send();
-    }
-
-    if (result.status === 'duplicated') {
-      return res.status(409).send();
-    }
-
-    res.status(201).json({
-      body: {
-        id: result.id,
-      },
-    });
+    res.status(201).json({ id });
   },
 
   deleteProduct(req: Request<IdParams>, res: Response) {
-    const reqId = req.params.id;
-
-    const result = productService.deleteProduct(reqId);
-
-    if (result.status === 'notFound') {
-      return res.status(404).send();
-    }
-
+    productService.deleteProduct(req.params.id);
     res.sendStatus(204);
   },
 };
