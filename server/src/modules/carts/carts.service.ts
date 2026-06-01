75e93f4a6d6dff9d85ddfdaf2005cdecb26e2c60
stardusty-lab <startdusty.lab@gmail.com>
2026-06-01 18:01:43 +0900

    feature: updateCartProduct 서비스 메서드의 유효성 검증 로직 추출
    Co-authored-by: stardusty-lab <stardusty-lab@users.noreply.github.com>
    Co-authored-by: kojesung <js95112345@gmail.com>
    Co-authored-by: 송아영 <azerosong@gmail.com>

--- server/src/modules/carts/carts.service.ts
+++ server/src/modules/carts/carts.service.ts
@@ -2,6 +2,8 @@ import * as cartsRepository from "./carts.repository.ts";
 import { ServiceError } from "../../common/error.ts";
 import { getMissingFields } from "../../validate/getMissingFields.ts";
 
+import * as validate from "./carts.validate.ts";
+
 export const getCartById = (cartId: number) => {
   const cart = cartsRepository.findById(cartId);
   if (!cart)
@@ -22,7 +24,7 @@ export const getCartById = (cartId: number) => {
   };
 };
 
-interface CartUpdateOption {
+export interface CartUpdateOption {
   quantity: number;
 }
 
@@ -31,47 +33,7 @@ export const updateCartProduct = (
   productId: number,
   cartUpdateOption: Partial<CartUpdateOption>,
 ) => {
-  const requiredFields = ["quantity"] as const;
-  const missingFields = getMissingFields(
-    cartUpdateOption,
-    requiredFields as unknown as string[],
-  );
-
-  if (missingFields.length > 0) {
-    throw new ServiceError(
-      "MISSING_FIELD",
-      "필수값이 누락되었습니다.",
-      missingFields.map((field) => ({
-        type: field,
-        errorCode: `MISSING_FIELD_${field.toUpperCase()}`,
-      })),
-    );
-  }
-
-  if (typeof cartUpdateOption.quantity !== "number") {
-    throw new ServiceError("TYPE_MISMATCH", "타입이 일치하지 않습니다.");
-  }
-
-  const invalidFields = [
-    {
-      type: "quantity",
-      isInvalid:
-        !Number.isInteger(cartUpdateOption.quantity) ||
-        cartUpdateOption.quantity! < 1 ||
-        cartUpdateOption.quantity! > 99,
-    },
-  ].filter(({ isInvalid }) => isInvalid);
-
-  if (invalidFields.length > 0) {
-    throw new ServiceError(
-      "INVALID",
-      "도메인 규칙에 맞지 않는 값입니다.",
-      invalidFields.map(({ type }) => ({
-        type,
-        errorCode: `INVALID_${type.toUpperCase()}`,
-      })),
-    );
-  }
+  validate.updateCartProduct(cartUpdateOption);
 
   const product = cartsRepository.updateProductQuantity(
     cartId,
