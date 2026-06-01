3fddacfbea3b8c608e40006e9928b6e135b237fe
binggwa <qudrhks317@gmail.com>
2026-06-01 15:27:14 +0900

    fix: 데이터 검증 시 암묵적 타입 변환으로 인해 문자열이 통과되는 버그 수정

--- server/src/repositories/util/Validator.ts
+++ server/src/repositories/util/Validator.ts
@@ -3,13 +3,16 @@ import { ERROR_MESSAGE } from "../../errors/ErrorMessage";
 import { ProductInput } from "../Product";
 
 export const validateQuantity = (quantity: number): void => {
-  if (quantity < 1 || quantity > 99)
+  if (Number.isNaN(quantity) || quantity < 1 || quantity > 99)
     throw new InvalidError(ERROR_MESSAGE.INVALID_QUANTITY_RANGE);
 };
 
 export const validateProductData = (data: ProductInput): void => {
   validateQuantity(data.totalQuantity);
-  if (!data.name || data.name.length > 100) throw new InvalidError(ERROR_MESSAGE.INVALID_NAME);
-  if (data.price <= 0) throw new InvalidError(ERROR_MESSAGE.INVALID_PRICE);
-  if (!data.thumbnailUrl) throw new InvalidError(ERROR_MESSAGE.INVALID_THUMBNAIL_URL);
+  if (!data.name || data.name.length > 100)
+    throw new InvalidError(ERROR_MESSAGE.INVALID_NAME);
+  if (typeof data.price !== "number" || Number.isNaN(data.price) || data.price <= 0)
+    throw new InvalidError(ERROR_MESSAGE.INVALID_PRICE);
+  if (!data.thumbnailUrl)
+    throw new InvalidError(ERROR_MESSAGE.INVALID_THUMBNAIL_URL);
 };
