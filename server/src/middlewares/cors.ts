590db9682122d0188507d2f97040ab32c19cbe92
janghw0126 <janghw232625@gmail.com>
2026-06-01 15:08:30 +0900

    fix: CORS 미들웨어에서 허용된 HTTP 메서드 목록에 PUT -> PATCH 수정

--- server/src/middlewares/cors.ts
+++ server/src/middlewares/cors.ts
@@ -2,7 +2,7 @@ import { Request, Response, NextFunction } from "express";
 
 const cors = (req: Request, res: Response, next: NextFunction) => {
   res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Methods", "GET,POST,PUT,DELETE,OPTIONS");
+  res.header("Access-Control-Allow-Methods", "GET,POST,PATCH,DELETE,OPTIONS");
   res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
   if (req.method === "OPTIONS") {
     res.sendStatus(204);
