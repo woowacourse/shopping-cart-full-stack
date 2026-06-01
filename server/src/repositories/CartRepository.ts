d739e47187486f8469a6ba10374678d75706659d
binggwa <qudrhks317@gmail.com>
2026-06-01 13:04:22 +0900

    fix: 2중 export 제거(싱글톤) 및 clear()메서드 제거 - 추후 의존성 주입을 통해 테스트오류 해결예정

--- server/src/repositories/CartRepository.ts
+++ server/src/repositories/CartRepository.ts
@@ -10,17 +10,14 @@ export default class CartRepository {
     this.#nextId = 1;
   }
 
-  // cartItemId를 기반으로 장바구니의 목록 1개 집어오기
   findById(cartItemId: number): CartItemData | null {
     return this.#cart.get(cartItemId) ?? null;
   }
 
-  // `getCartProducts()` 장바구니 목록 조회
   getCartProducts(): CartItemData[] {
     return [...this.#cart.values()];
   }
 
-  // `addProductToCart()` 장바구니 목록 추가
   addProductToCart(productId: number, quantity: number): CartItemData {
     validateQuantity(quantity);
     const cartItem = { productId, quantity, cartItemId: this.#nextId };
@@ -29,35 +26,24 @@ export default class CartRepository {
     return cartItem;
   }
 
-  // `changeQuantity()` 장바구니 수량 변경
   changeQuantity(cartItemId: number, newQuantity: number): CartItemData | null {
     validateQuantity(newQuantity);
-    // cartItemId를 찾아서
     const cartItem = this.#cart.get(cartItemId);
     if (!cartItem) return null;
-    // newQuantity로 업데이트
+
     const newItem = { ...cartItem, quantity: newQuantity };
     this.#cart.set(cartItemId, newItem);
 
     return newItem;
   }
 
-  // ` deleteByCartId()` 장바구니 목록 제거
   deleteByCartId(cartItemId: number): void {
     this.#cart.delete(cartItemId);
   }
 
-  // ` deleteByProductId()` cascading 제거
   deleteByProductId(productId: number): void {
     [...this.#cart.entries()]
       .filter(([_, cartItemData]) => cartItemData.productId === productId)
       .forEach(([cartItemId]) => this.#cart.delete(cartItemId));
   }
-
-  clear(): void {
-    this.#cart.clear();
-    this.#nextId = 1;
-  }
 }
-
-export const cartRepository = new CartRepository();
