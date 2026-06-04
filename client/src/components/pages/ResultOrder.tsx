82bd5c8fe134f8096487631d19a1b72bca8bdfdd
김상윤 <ksy151020@gmail.com>
2026-06-04 17:27:46 +0900

    feat: ResultOrder 컴포넌트 구현

--- client/src/components/pages/ResultOrder.tsx
+++ client/src/components/pages/ResultOrder.tsx
@@ -1,3 +1,32 @@
-export default function ResultOrder() {
-  return <div>ResultOrder</div>;
+interface Props {
+  orderPrice: number;
+  deliveryPrice: number;
+  totalPrice: number;
+}
+
+export default function ResultOrder({
+  orderPrice,
+  deliveryPrice,
+  totalPrice,
+}: Props) {
+  return (
+    <table>
+      <th>
+        <img src="/!_img.jpg" alt="느낌표" />
+        <p>총 주문 금액이 100,000원 이상일 경우 무료 배송됩니다.</p>
+      </th>
+      <tr>
+        <td>주문 금액</td>
+        <td>{orderPrice}</td>
+      </tr>
+      <tr>
+        <td>배송비</td>
+        <td>{deliveryPrice}</td>
+      </tr>
+      <tr>
+        <td>총 결제 금액</td>
+        <td>{totalPrice}</td>
+      </tr>
+    </table>
+  );
 }
