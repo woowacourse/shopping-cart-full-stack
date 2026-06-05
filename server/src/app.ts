7b8de9e79226ab2a61d8d2a384def4cd463e0240
김상윤 <ksy151020@gmail.com>
2026-06-05 15:31:01 +0900

    chore: Vercel 배포 설정 및 CORS 도메인 제한

--- server/src/app.ts
+++ server/src/app.ts
@@ -1,11 +1,15 @@
 import express from "express";
-import cors from 'cors';
-import cartRouter from './routes/CartRouter';
-import productRouter from './routes/ProductRouter';
+import cors from "cors";
+import cartRouter from "./routes/CartRouter";
+import productRouter from "./routes/ProductRouter";
 
 const app = express();
 
-app.use(cors());
+app.use(
+  cors({
+    origin: "https://shopping-cart-full-stack-sigma.vercel.app/",
+  }),
+);
 app.use(express.json());
 app.use("/cart", cartRouter);
 app.use("/products", productRouter);
