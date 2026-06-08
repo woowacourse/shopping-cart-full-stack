460876d0765e94e754981624d87abe0fdff1f746
김상윤 <ksy151020@gmail.com>
2026-06-08 16:27:11 +0900

    feat: PATCH 요청시 debounce 구현

--- client/src/hooks/useCartItmes.ts
+++ client/src/hooks/useCartItmes.ts
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import useFetch from "./useFetch";
 import { CartItem } from "../type/types";
 import { shoppingCartApi, BASE_URL } from "../api/shoppingCartApi";
@@ -23,20 +23,27 @@ export default function useCartItmes() {
     }
   };
 
-  const onQuantityChange = async (cartItemId: number, newQuantity: number) => {
-    try {
-      const res = await shoppingCartApi.patch(cartItemId, newQuantity);
-      if (!res.ok) throw new Error();
-      const newCartItems = cartItems.map((item) => {
-        if (item.cartItemId === cartItemId) {
-          return { ...item, quantity: newQuantity };
-        }
-        return item;
-      });
-      setCartItems(newCartItems);
-    } catch {
-      alert(ERROR_MESSAGES.PATCH_ERROR_MESSAGE);
-    }
+  const timeRef = useRef<ReturnType<typeof setTimeout> | null>(null);
+
+  const onQuantityChange = (cartItemId: number, newQuantity: number) => {
+    const prevCartItems = cartItems;
+    const newCartItems = cartItems.map((item) => {
+      return item.cartItemId === cartItemId
+        ? { ...item, quantity: newQuantity }
+        : item;
+    });
+
+    setCartItems(newCartItems);
+    if (timeRef.current) clearTimeout(timeRef.current);
+    timeRef.current = setTimeout(async () => {
+      try {
+        const res = await shoppingCartApi.patch(cartItemId, newQuantity);
+        if (!res.ok) throw new Error();
+      } catch {
+        setCartItems(prevCartItems);
+        alert(ERROR_MESSAGES.PATCH_ERROR_MESSAGE);
+      }
+    }, 500);
   };
   return { state, cartItems, onDelete, onQuantityChange };
 }
