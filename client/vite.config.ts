565880940eab221f22c8271e3373db2b11c1c2a9
김상윤 <ksy151020@gmail.com>
2026-06-08 17:17:58 +0900

    chore: msw RTL테스트를 위한 환경 세팅

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,4 +1,5 @@
-import { defineConfig } from "vite";
+/// <reference types="vitest" />
+import { defineConfig } from "vitest/config";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
@@ -8,5 +9,10 @@ export default defineConfig({
       "/cart": "http://localhost:3000",
       "/products": "http://localhost:3000",
     },
+  },
+  test: {
+    globals: true,
+    environment: "jsdom",
+    setupFiles: "./src/test/setup.ts",
   },
 });
