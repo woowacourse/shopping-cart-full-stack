cc9931f634ac4bb59736626c9d3672059833d51e
김상윤 <ksy151020@gmail.com>
2026-06-15 13:20:00 +0900

    feat: order 에러메시지 추가

--- server/src/errors/ErrorMessage.ts
+++ server/src/errors/ErrorMessage.ts
@@ -1,8 +1,10 @@
 export const ERROR_MESSAGE = {
   NOT_FOUND_PRODUCT: "해당 상품이 존재하지 않습니다.",
   NOT_FOUND_CART_ITEM: "해당 장바구니 상품이 존재하지 않습니다.",
+  NOT_FOUND_ORDER: "해당 주문이 존재하지 않습니다.",
   INVALID_CART_ID: "유효하지 않은 장바구니 ID입니다.",
   INVALID_PRODUCT_ID: "유효하지 않은 상품 ID입니다.",
+  INVALID_ORDER_ID: "유효하지 않은 주문 ID입니다.",
   INVALID_QUANTITY: "유효하지 않은 수량입니다.",
   INVALID_QUANTITY_RANGE: "quantity는 1~99 사이어야합니다.",
   INVALID_PRICE: "price는 0보다 큰 숫자이어야합니다.",
