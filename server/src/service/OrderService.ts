e4ce62931456e2033fa1a6ae2a0823950c0c84bd
김상윤 <ksy151020@gmail.com>
2026-06-15 13:20:28 +0900

    feat: OrderService 구현

---
+++ server/src/service/OrderService.ts
@@ -0,0 +1,29 @@
+import { InvalidError, NotFoundError } from "../errors/CustomErrorClass";
+import { ERROR_MESSAGE } from "../errors/ErrorMessage";
+import { StoredOrder } from "../repositories/StoredOrder";
+import { storedOrderRepository } from "../repositories/OrderRepository";
+
+export const getOrdersService = (orderId: number): StoredOrder => {
+  const order = storedOrderRepository.findById(orderId);
+  if (!order)
+    throw new NotFoundError("NOT_FOUND_ORDER", ERROR_MESSAGE.NOT_FOUND_ORDER);
+  return order;
+};
+
+export const postOrderService = (
+  newOrder: Omit<StoredOrder, "orderId">,
+): StoredOrder => {
+  const addedOrder = storedOrderRepository.addOrder(newOrder);
+  return addedOrder;
+};
+
+export const deleteOrderService = (orderId: number): void => {
+  if (!orderId)
+    throw new InvalidError("INVALID_ORDER_ID", ERROR_MESSAGE.INVALID_ORDER_ID);
+
+  const order = storedOrderRepository.findById(orderId);
+  if (!order)
+    throw new NotFoundError("NOT_FOUND_ORDER", ERROR_MESSAGE.NOT_FOUND_ORDER);
+
+  storedOrderRepository.deleteById(orderId);
+};
