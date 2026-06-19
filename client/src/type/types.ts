4186b74b5ebc725ff0f998e1e569a2085509a454
김상윤 <ksy151020@gmail.com>
2026-06-19 13:13:10 +0900

    feat: OrderData, CouponData 타입 정의

--- client/src/type/types.ts
+++ client/src/type/types.ts
@@ -10,3 +10,26 @@ export interface CartItem {
     totalQuantity: number;
   };
 }
+
+export interface OrderData {
+  orderId: number;
+  items: Array<{ productId: number; quantity: number }>;
+  appliedCoupon: number[];
+  remoteArea: boolean; //제주도, 도서산간 지역
+  orderAmount: number;
+  couponDiscountAmount: number;
+  shippingFee: number;
+  totalAmount: number;
+}
+
+export interface CouponData {
+  couponId: number;
+  couponCode: string; // 'FIXED5000' | 'BTGO' | 'FREESHIPPING' | 'MIRACLESALE'
+  expiredDate: string;
+  minOrderAmount?: number;
+  usableStartAt?: string | null;
+  usableEndAt?: string | null;
+  discountAmount?: number;
+  discountRate?: number;
+  isAvailable?: boolean;
+}
