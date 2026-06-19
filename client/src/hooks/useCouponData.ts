82dacbbe42192428e15636e76a8cf062b1408edf
김상윤 <ksy151020@gmail.com>
2026-06-19 17:25:44 +0900

    feat: CouponData를 관리하는 훅 useCouponData 생성

---
+++ client/src/hooks/useCouponData.ts
@@ -0,0 +1,18 @@
+import { useEffect, useState } from "react";
+import { CouponData, OrderData } from "../type/types";
+import useFetch from "./useFetch";
+import { couponApi } from "../api/couponApi";
+
+export default function useCouponData(orderId: number) {
+  const { state, fetchData } = useFetch<CouponData[]>(() =>
+    couponApi.get(orderId),
+  );
+  const [couponData, setCouponData] = useState<CouponData[]>([]);
+
+  useEffect(() => {
+    if (state.status !== "success") return;
+    setCouponData(state.data);
+  }, [state]);
+
+  return { state, fetchData, couponData };
+}
