3440c247f24dea3688cefbdf529818dac9214b8e
김상윤 <ksy151020@gmail.com>
2026-06-20 17:00:30 +0900

    feat: OrderCartItem 컴포넌트 구현

---
+++ client/src/components/order/OrderCartItem.tsx
@@ -0,0 +1,48 @@
+import styled from "styled-components";
+import type { CartItem } from "../../type/types";
+
+interface Props {
+  cartItem: CartItem;
+}
+
+export default function ShoppingCartItem({ cartItem }: Props) {
+  const { productData, quantity } = cartItem;
+  return (
+    <div>
+      <Container>
+        <ItemContainer>
+          <img width={112} height={112} src={productData.thumbnailUrl} />
+          <ItemInfoContainer>
+            <Name>{productData.name}</Name>
+            <Price>{productData.price.toLocaleString()}원</Price>
+            <Quantity>수량 {quantity}</Quantity>
+          </ItemInfoContainer>
+        </ItemContainer>
+      </Container>
+    </div>
+  );
+}
+
+const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  width: 100%;
+  border-top: solid 1px #0000001a;
+`;
+
+const ItemContainer = styled.div`
+  display: flex;
+  flex-direction: row;
+  align-items: flex-start;
+`;
+const ItemInfoContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  margin-left: 24px;
+`;
+
+const Name = styled.p``;
+
+const Price = styled.p``;
+
+const Quantity = styled.p``;
