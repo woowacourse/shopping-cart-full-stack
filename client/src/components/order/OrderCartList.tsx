43298cf23cb755bbe35b270c5486eef0afdf7079
김상윤 <ksy151020@gmail.com>
2026-06-20 18:02:09 +0900

    feat: OrderCartList 컴포넌트 구현

--- client/src/components/order/OrderCartList.tsx
+++ client/src/components/order/OrderCartList.tsx
@@ -1,18 +1,18 @@
 import styled from "styled-components";
-import { OrderData } from "../../type/types";
+import { OrderItem } from "../../type/types";
+import OrderCartItem from "./OrderCartItem";
 
 interface Props {
-  orderDatas: OrderData[];
+  items: OrderItem[];
 }
-
-export default function OrderCartList({ orderDatas }: Props) {
+export default function OrderCartList({ items }: Props) {
   //cartList에서 isSelect인 애들만 끌고옴
   // 뭘 끌고 오냐 써네일, 이름, 가격, 수량
   // 수량 버튼 빼고는 cartItem 컴포넌트와 똑같이 생김
   return (
     <Container>
-      {orderDatas.map((orderData) => {
-        return <OrderCartItem orderItem={orderData} />;
+      {items.map((item) => {
+        return <OrderCartItem key={item.productId} item={item} />;
       })}
     </Container>
   );
@@ -20,7 +20,7 @@ export default function OrderCartList({ orderDatas }: Props) {
 
 const Container = styled.div`
   display: flex;
-  flex-direction: column; // 추가
+  flex-direction: column;
   width: 100%;
   gap: 20px;
   flex: 1;
