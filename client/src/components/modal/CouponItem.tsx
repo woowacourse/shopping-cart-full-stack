c7948509a2f4632b8294c4ddcead1e2eff89c8c8
김상윤 <ksy151020@gmail.com>
2026-06-21 23:30:21 +0900

    feat: CouponItem 컴포넌트 구현

---
+++ client/src/components/modal/CouponItem.tsx
@@ -0,0 +1,62 @@
+import styled from "styled-components";
+import { CouponData } from "../../type/types";
+
+interface Props {
+  couponData: CouponData;
+  isChecked: boolean;
+  isDisabled: boolean;
+  onToggle: (couponId: number) => void;
+}
+// couponCode ('FIXED5000' | 'BTGO' | 'FREESHIPPING' | 'MIRACLESALE')에 따라 5000원 할인 쿠폰,  2개 구매 시 1개 무료 쿠폰, 5만원 이상 구매 시 무료 배송 쿠폰, 미라클 모닝 30% 할인 쿠폰 이 couponName
+
+const COUPON_NAME: Record<string, string> = {
+  FIXED5000: "5,000원 할인 쿠폰",
+  BTGO: "2개 구매 시 1개 무료 쿠폰",
+  FREESHIPPING: "5만원 이상 구매 시 무료 배송 쿠폰",
+  MIRACLESALE: "미라클 모닝 30% 할인 쿠폰",
+};
+
+const formatDate = (dateStr: string) => {
+  const [year, month, day] = dateStr.split("-");
+  return `${year}년 ${month}월 ${day}일`;
+};
+
+export default function CouponItem({
+  isChecked,
+  couponData,
+  isDisabled,
+  onToggle,
+}: Props) {
+  return (
+    <Container isDisabled={isDisabled}>
+      <TopSection>
+        <input
+          type="checkbox"
+          checked={isChecked}
+          disabled={isDisabled}
+          onChange={() => onToggle(couponData.couponId)}
+        />
+        <CouponName>{COUPON_NAME[couponData.couponCode]}</CouponName>
+      </TopSection>
+      <InfoLine>만료일: {formatDate(couponData.expiredDate)}</InfoLine>
+      {couponData.minOrderAmount && (
+        <InfoLine>
+          최소 주문 금액: {couponData.minOrderAmount.toLocaleString()}원{" "}
+        </InfoLine>
+      )}
+      {couponData.usableStartAt && couponData.usableEndAt && (
+        <InfoLine>
+          사용 가능 시간: 오전{couponData.usableStartAt}시 부터{" "}
+          {couponData.usableEndAt}시 까지
+        </InfoLine>
+      )}
+    </Container>
+  );
+}
+
+const Container = styled.div<{ isDisabled: boolean }>`
+  opacity: ${({ isDisabled }) => (isDisabled ? 0.4 : 1)};
+`;
+const CouponName = styled.p``;
+const TopSection = styled.div``;
+const InfoLine = styled.p``;
