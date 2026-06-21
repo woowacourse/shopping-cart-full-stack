a80e0575ea5ac566d496a3204ce8d3532a11bbc0
JetProc <soonjae8297@gmail.com>
2026-06-22 04:49:01 +0900

    refactor: 장바구니 성공 화면 구조 단순화

--- client/src/domains/cart/components/cart-page/CartSuccessView.tsx
+++ client/src/domains/cart/components/cart-page/CartSuccessView.tsx
@@ -1,14 +1,27 @@
 import {PageIntro} from '../../../../shared/layout/PageIntro.js';
 import {useCart} from '../../hooks/useCart.js';
-import {CartSuccessContent} from './CartSuccessContent.js';
+import {getSelectedOrderAmount, getShippingFee, getTotalPrice} from '../../domain/cartSelectors.js';
+import {CartItemList} from '../cart-item/CartItemList.js';
+import {PaymentSummary} from './PaymentSummary.js';
 
 export const CartSuccessView = () => {
-  const {cartItemsState} = useCart();
+  const {cartItemsState, changeCartItemQuantity, changeSelectedCartItemIds, removeCartItem, selectedIds} = useCart();
+  const cartSelection = {items: cartItemsState.items, selectedIds};
+  const selectedOrderAmount = getSelectedOrderAmount(cartSelection);
+  const shippingFee = getShippingFee(cartSelection);
+  const totalPrice = getTotalPrice(cartSelection);
 
   return (
     <>
       <PageIntro title='장바구니' description={`현재 ${cartItemsState.items.length}종류의 상품이 담겨있습니다.`} />
-      <CartSuccessContent />
+      <CartItemList
+        items={cartItemsState.items}
+        selectedIds={selectedIds}
+        onChangeQuantity={changeCartItemQuantity}
+        onChangeSelectedIds={changeSelectedCartItemIds}
+        onDelete={removeCartItem}
+      />
+      <PaymentSummary selectedOrderAmount={selectedOrderAmount} shippingFee={shippingFee} totalPrice={totalPrice} />
     </>
   );
 };
