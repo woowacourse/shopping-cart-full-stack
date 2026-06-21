7d2c37ad0e5f7729b507fbcf45430fd930955e3c
JetProc <soonjae8297@gmail.com>
2026-06-21 21:04:09 +0900

    refactor: 장바구니 인트로 공통화

--- client/src/cart/pages/CartPage.tsx
+++ client/src/cart/pages/CartPage.tsx
@@ -1,15 +1,13 @@
-import styled from '@emotion/styled';
-
-import {AsyncStateView} from '../../design-system/index.js';
+import {AsyncStateView, Typo} from '../../design-system/index.js';
 
 import {CartItemList} from '../components/cart-item/CartItemList.js';
 import {CartEmptyView} from '../components/cart-page/CartEmptyView.js';
 import {CartErrorView} from '../components/cart-page/CartErrorView.js';
 import {CartLoadingView} from '../components/cart-page/CartLoadingView.js';
 import {CartOrderAction} from '../components/cart-page/CartOrderAction.js';
-import {CartPageHeader} from '../components/cart-page/CartPageHeader.js';
 import {PaymentSummary} from '../components/cart-page/PaymentSummary.js';
-import {Header} from '../components/layout/Header.js';
+import {PageIntro} from '../../layout/PageIntro.js';
+import {ScreenLayout} from '../../layout/ScreenLayout.js';
 
 import {useCart} from '../hooks/useCart.js';
 import {getSelectedOrderAmount, getShippingFee, getTotalPrice} from '../domain/cartSelectors.js';
@@ -18,24 +16,29 @@ import type {CartItemsState} from '../domain/types.js';
 export const CartPage = () => {
   const {cartItemsState, loadCartItems} = useCart();
   const status = getCartPageStatus(cartItemsState);
-  const isOrderActionVisible = status === 'success' || status === 'empty';
 
   return (
-    <>
-      <Header title='SHOP' />
-      <Main>
-        <CartPageHeader itemCount={status === 'success' ? cartItemsState.items.length : null} />
-        <AsyncStateView
-          emptyFallback={<CartEmptyView />}
-          errorFallback={<CartErrorView errorMessage={cartItemsState.errorMessage} onRetry={loadCartItems} />}
-          loadingFallback={<CartLoadingView />}
-          status={status}
-        >
-          <CartPageContent />
-        </AsyncStateView>
-        {isOrderActionVisible && <CartOrderAction />}
-      </Main>
-    </>
+    <ScreenLayout
+      header={
+        <Typo as='span' color='white' variant='headline' weight='bold'>
+          SHOP
+        </Typo>
+      }
+      bottomButton={<CartOrderAction />}
+    >
+      <PageIntro
+        title='장바구니'
+        description={status === 'success' ? `현재 ${cartItemsState.items.length}종류의 상품이 담겨있습니다.` : null}
+      />
+      <AsyncStateView
+        emptyFallback={<CartEmptyView />}
+        errorFallback={<CartErrorView errorMessage={cartItemsState.errorMessage} onRetry={loadCartItems} />}
+        loadingFallback={<CartLoadingView />}
+        status={status}
+      >
+        <CartPageContent />
+      </AsyncStateView>
+    </ScreenLayout>
   );
 };
 
@@ -65,12 +68,3 @@ function getCartPageStatus(state: CartItemsState) {
 
   return state.status;
 }
-
-const Main = styled.main`
-  display: flex;
-  min-height: calc(100dvh - 64px);
-  box-sizing: border-box;
-  flex-direction: column;
-  padding: 36px 24px;
-  padding-bottom: calc(64px + 32px);
-`;
