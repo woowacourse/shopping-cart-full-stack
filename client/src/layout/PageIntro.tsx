4baac538188d1f30d66f901a813784fa4c4e7117
JetProc <soonjae8297@gmail.com>
2026-06-21 21:03:30 +0900

    refactor: 페이지 인트로 컴포넌트 추가

---
+++ client/src/layout/PageIntro.tsx
@@ -0,0 +1,30 @@
+import styled from '@emotion/styled';
+import type {ReactNode} from 'react';
+
+import {Typo} from '../design-system/index.js';
+
+interface PageIntroProps {
+  title: ReactNode;
+  description?: ReactNode;
+}
+
+export const PageIntro = ({title, description}: PageIntroProps) => {
+  return (
+    <Container>
+      <Typo as='h1' variant='display' weight='bold'>
+        {title}
+      </Typo>
+      {description && (
+        <Typo as='p' color='gray900' variant='caption' weight='medium'>
+          {description}
+        </Typo>
+      )}
+    </Container>
+  );
+};
+
+const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 16px;
+`;
