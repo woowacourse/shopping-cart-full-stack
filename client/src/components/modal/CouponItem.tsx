bf003aa155f0d7151cdfeca300bd071936fd0313
김상윤 <ksy151020@gmail.com>
2026-06-22 04:33:59 +0900

    refactor: 쿠폰 코드: 쿠폰 이름 객체 상수 constants 파일로 분리

--- client/src/components/modal/CouponItem.tsx
+++ client/src/components/modal/CouponItem.tsx
@@ -1,5 +1,6 @@
 import styled from "styled-components";
 import { CouponData } from "../../type/types";
+import { COUPON_NAME } from "../../constants/constants";
 
 interface Props {
   couponData: CouponData;
@@ -7,14 +8,6 @@ interface Props {
   isDisabled: boolean;
   onToggle: (couponId: number) => void;
 }
-// couponCode ('FIXED5000' | 'BTGO' | 'FREESHIPPING' | 'MIRACLESALE')에 따라 5000원 할인 쿠폰,  2개 구매 시 1개 무료 쿠폰, 5만원 이상 구매 시 무료 배송 쿠폰, 미라클 모닝 30% 할인 쿠폰 이 couponName
-
-const COUPON_NAME: Record<string, string> = {
-  FIXED5000: "5,000원 할인 쿠폰",
-  BTGO: "2개 구매 시 1개 무료 쿠폰",
-  FREESHIPPING: "5만원 이상 구매 시 무료 배송 쿠폰",
-  MIRACLESALE: "미라클 모닝 30% 할인 쿠폰",
-};
 
 const formatDate = (dateStr: string) => {
   const [year, month, day] = dateStr.split("-");
